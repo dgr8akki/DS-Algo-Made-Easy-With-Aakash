f35b76973a47a8d5f14d00c13bf34400c0b2e3f2
Aakash Pahuja <aakash.pahuja@kony.com>
2018-08-12 21:52:51 +0530

    Add folders for hackerrank and leetcode

---
+++ Hackerrank/JesseAndCookies.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+class Solution {
+  static int cookies(int k, int[] A) {
+    int temp[] = new int[A.length];
+    int count = -1;
+    for(int i = 0; i < A.length; i++)
+      temp[i] = A[i];
+    Arrays.sort(temp);
+    while (temp[0] < k) {
+      count++;
+      int b = 1 * temp[0] + 2 * temp[1];
+      temp[0] = b;
+      temp[1] = Integer.MAX_VALUE;
+      Arrays.sort(temp);
+    }
+    return count;
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    String[] nk = scanner.nextLine().split(" ");
+
+    int n = Integer.parseInt(nk[0].trim());
+
+    int k = Integer.parseInt(nk[1].trim());
+
+    int[] A = new int[n];
+
+    String[] AItems = scanner.nextLine().split(" ");
+
+    for (int AItr = 0; AItr < n; AItr++) {
+      int AItem = Integer.parseInt(AItems[AItr].trim());
+      A[AItr] = AItem;
+    }
+
+    int result = cookies(k, A);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedWriter.close();
+  }
+}
