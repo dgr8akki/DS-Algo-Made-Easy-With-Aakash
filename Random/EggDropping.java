dfa251ce3646e8743188a501ca0eead61428a009
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-17 00:15:02 +0530

    Refactor egg dropping problem

--- Random/EggDropping.java
+++ Random/EggDropping.java
@@ -1,28 +1,23 @@
 public class EggDropping {
-  public int calculateAttempts (int totalEggs, int totalFloors) {
+  public int calculateAttempts(int totalEggs, int totalFloors) {
     int[][] attempts = new int[totalEggs + 1][totalFloors + 1];
-    int attempt = 0;
-    for(int i = 0; i <= totalFloors; i++)
+    for (int i = 0; i <= totalFloors; i++)
       attempts[1][i] = i;
 
-    for(int eggIndex = 2; eggIndex <= totalEggs; eggIndex++) {
-      for(int floorIndex = 1; floorIndex <= totalFloors; floorIndex++) {
+    for (int eggIndex = 2; eggIndex <= totalEggs; eggIndex++) {
+      for (int floorIndex = 1; floorIndex <= totalFloors; floorIndex++) {
         attempts[eggIndex][floorIndex] = Integer.MAX_VALUE;
-
-        for(int k = 1; k <= floorIndex; k++) {
-          attempt = 1 + Math.max(attempts[eggIndex - 1][k - 1], attempts[eggIndex][floorIndex - k]);
-
-          if(attempt < attempts[eggIndex][floorIndex]) {
-            attempts[eggIndex][floorIndex] = attempt;
-          }
-        }
+        for (int k = 1; k <= floorIndex; k++)
+          attempts[eggIndex][floorIndex] = Math.min(attempts[eggIndex][floorIndex],
+              1 + Math.max(attempts[eggIndex - 1][k - 1], attempts[eggIndex][floorIndex - k]));
       }
     }
-  return attempts[totalEggs][totalFloors];
+    return attempts[totalEggs][totalFloors];
   }
 
   public static void main(String[] args) {
     EggDropping eggDropping = new EggDropping();
-    System.out.println("Minimum " + eggDropping.calculateAttempts(2, 100) + " attempts needed, if you have 2 eggs and 100 floor building.");
+    System.out.println("Minimum " + eggDropping.calculateAttempts(2, 100)
+        + " attempts needed, if you have 2 eggs and 100 floor building.");
   }
 }
