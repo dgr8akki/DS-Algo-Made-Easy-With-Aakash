10bcf9fd3003328b98c2518efa34917938839d13
Aakash Pahuja <aakash.pahuja@kony.com>
2018-11-17 12:07:32 +0530

    [Graph] Add dfs in graph using Objects

--- Ds&AlgoMadeEasyInJava/Graph/GraphUsingObjects.java
+++ Ds&AlgoMadeEasyInJava/Graph/GraphUsingObjects.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class GraphUsingObjects {
   class Vertex {
     int data;
-    ArrayList<Integer> neighbours;
+    ArrayList<Vertex> neighbours;
 
     Vertex(int data) {
       this.data = data;
@@ -22,17 +23,25 @@ public class GraphUsingObjects {
     }
   }
 
+  public Vertex getNodeVertex (int data) {
+    for(Vertex current : vertices) {
+      if(current.data == data)
+        return current;
+    }
+    return null;
+  }
+
   public void addEdge(int src, int dest) {
-    vertices.get(src).neighbours.add(dest);
-    vertices.get(dest).neighbours.add(src);
+    vertices.get(src).neighbours.add(new Vertex(dest));
+    vertices.get(dest).neighbours.add(new Vertex(src));
   }
 
   public void removeEdge(int src, int dest) {
-    vertices.get(src).neighbours.remove((Object)dest);
-    vertices.get(dest).neighbours.remove((Object)src);
+    vertices.get(src).neighbours.remove(getNodeVertex(dest));
+    vertices.get(dest).neighbours.remove(getNodeVertex(src));
   }
 
-    public void print() {
+  public void print() {
     for(int index = 0; index < vertices.size(); index++) {
       for(int jindex = -1; jindex < vertices.get(index).neighbours.size(); jindex++) {
         if(jindex == -1) {
@@ -40,12 +49,32 @@ public class GraphUsingObjects {
           continue;
         }
         if(jindex == 0) {
-          System.out.print(vertices.get(index).neighbours.get(jindex));
+          System.out.print(vertices.get(index).neighbours.get(jindex).data);
           continue;
         }
-        System.out.print(" , " + vertices.get(index).neighbours.get(jindex));
+        System.out.print(" , " + vertices.get(index).neighbours.get(jindex).data);
       }
       System.out.println();
+    }
+  }
+
+  public void dfs(int data) {
+    Vertex startNode = getNodeVertex(data);
+    Stack<Vertex> stack = new Stack<>();
+    boolean[] isVisited = new boolean[verticesCount];
+    stack.push(startNode);
+
+    while(!stack.isEmpty()){
+      Vertex currentNode = stack.pop();
+      if(!isVisited[currentNode.data]) {
+        System.out.print(currentNode.data + " => ");
+        isVisited[currentNode.data] = true;
+      }
+
+      for(Vertex node : currentNode.neighbours) {
+        if(!isVisited[node.data])
+          stack.push(node);
+      }
     }
   }
   public static void main(String[] args) {
@@ -58,5 +87,6 @@ public class GraphUsingObjects {
       graph.addEdge(2, 4);
       graph.removeEdge(2, 4);
       graph.print();
+      graph.dfs(1);
   }
 }
