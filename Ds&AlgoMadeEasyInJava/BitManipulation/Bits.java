0b5c3ae28753d77354cab8dfbb1ad2cb0d5528c9
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-01 19:02:32 +0530

    [Bit Manipulation] Add kth bit manipulation methods

--- Ds&AlgoMadeEasyInJava/BitManipulation/Bits.java
+++ Ds&AlgoMadeEasyInJava/BitManipulation/Bits.java
@@ -56,6 +56,76 @@ public class Bits {
     System.out.println("--------------------------------------");
   }
 
+  public void isKthBitSet (int number, int k) {
+    printInBinary(number);
+    System.out.println("Is bit number "+ k +" of "+ number +", is set(Set means bit is 1)?");
+    /**
+     * Checking is Kth Bit set
+     *
+     * Algorithm:
+     * Step 1. Left shift 1 by k-1 bits
+     * Step 2. AND the above result by original number.
+     *
+     * Pseudo code:
+     * number & (1 << k - 1)
+     */
+    System.out.println((number & (1 << (k - 1))) != 0);
+    // printInBinary(number & (1 << (k - 1)));
+    System.out.println("--------------------------------------");
+  }
+
+  public void setKthBit (int number, int k) {
+    printInBinary(number);
+    System.out.println("After setting bit number "+ k +" of "+ number +", It becomes:");
+    /**
+     * Setting Kth Bit of number
+     *
+     * Algorithm:
+     * Step 1. Left shift 1 by k-1 bits
+     * Step 2. OR the above result by original number.
+     *
+     * Pseudo code:
+     * number & (1 << k - 1)
+     */
+    printInBinary(number | (1 << (k - 1)));
+    System.out.println("--------------------------------------");
+  }
+
+  public void clearKthBit (int number, int k) {
+    printInBinary(number);
+    System.out.println("After clearing bit number "+ k +" of "+ number +", It becomes:");
+    /**
+     * Clearing Kth Bit of number
+     *
+     * Algorithm:
+     * Step 1. Left shift 1 by k-1 bits
+     * Step 2. Negate or compliment the above result.
+     * Step 3. AND the above result by original number.
+     *
+     * Pseudo code:
+     * number & ~(1 << k - 1)
+     */
+    printInBinary(number & ~(1 << (k - 1)));
+    System.out.println("--------------------------------------");
+  }
+
+  public void toggleKthBit (int number, int k) {
+    printInBinary(number);
+    System.out.println("After toggling bit number "+ k +" of "+ number +", It becomes:");
+    /**
+     * Toggling Kth Bit of number
+     *
+     * Algorithm:
+     * Step 1. Left shift 1 by k-1 bits
+     * Step 2. XOR the above result by original number.
+     *
+     * Pseudo code:
+     * number ^ (1 << k - 1)
+     */
+    printInBinary(number ^ (1 << (k - 1)));
+    System.out.println("--------------------------------------");
+  }
+
   public static void main(String[] args) {
     Bits bits = new Bits();
     int first = 125;
@@ -68,5 +138,10 @@ public class Bits {
     bits.bitwiseRightShift(first, k);
     bits.bitwiseCompliment(first);
     bits.bitwiseTwosCompliment(first);
+    bits.isKthBitSet(first, 2);
+    bits.isKthBitSet(first, 4);
+    bits.setKthBit(first, k);
+    bits.clearKthBit(first, 4);
+    bits.toggleKthBit(first, k);
   }
 }
