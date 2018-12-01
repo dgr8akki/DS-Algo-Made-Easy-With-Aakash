07616c0db3514514e87bb7f737178b7ccf9a31f9
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-01 19:54:56 +0530

    [Bit Manipulation] Add rightmost bit manipulation functions

--- Ds&AlgoMadeEasyInJava/BitManipulation/Bits.java
+++ Ds&AlgoMadeEasyInJava/BitManipulation/Bits.java
@@ -126,6 +126,60 @@ public class Bits {
     System.out.println("--------------------------------------");
   }
 
+  public void toggleRightmostOneBit (int number) {
+    printInBinary(number);
+    System.out.println("After toggling rightmost one bit of "+ number +", It becomes:");
+    /**
+     * Toggling Rightmost one Bit of number
+     * It will toggle the first 1 from right most
+     * eg. 1111100 => 1111000 (Rightmost 1 is at 3rd bit from last)
+     *
+     * Algorithm:
+     * Step 1. AND actual number with one less of the same number
+     *
+     * Pseudo code:
+     * number & (number - 1)
+     */
+    printInBinary(number & (number - 1));
+    System.out.println("--------------------------------------");
+  }
+
+  public void isolateRightmostOneBit (int number) {
+    printInBinary(number);
+    System.out.println("After isolating rightmost one bit of "+ number +", It becomes:");
+    /**
+     * Isolating Rightmost one Bit of number
+     * It will isolate the first 1 from right most means it will preserve last 1 from right and clear all bits.
+     *
+     * Algorithm:
+     * Step 1. Negate or two's compliment the original number
+     * Step 2. AND above result original number
+     *
+     * Pseudo code:
+     * (number) & (~number + 1)
+     */
+    printInBinary(number & ((~ number) + 1));
+    System.out.println("--------------------------------------");
+  }
+
+  public void isolateRightmostZeroBit (int number) {
+    printInBinary(number);
+    System.out.println("After isolating rightmost Zero bit of "+ number +", It becomes:");
+    /**
+     * Isolating Rightmost Zero Bit of number
+     * It will isolate the first 0 from right most means it will preserve last 0 from right and clear all bits after it.
+     *
+     * Algorithm:
+     * Step 1. Negate or two's compliment the original number
+     * Step 2. AND above result with actual number plus one
+     *
+     * Pseudo code:
+     * (number + 1) & (~number + 1)
+     */
+    printInBinary((number + 1) & ((~ number) + 1));
+    System.out.println("--------------------------------------");
+  }
+
   public static void main(String[] args) {
     Bits bits = new Bits();
     int first = 125;
@@ -143,5 +197,10 @@ public class Bits {
     bits.setKthBit(first, k);
     bits.clearKthBit(first, 4);
     bits.toggleKthBit(first, k);
+    bits.toggleRightmostOneBit(124);
+    bits.isolateRightmostOneBit(first);
+    bits.isolateRightmostOneBit(124);
+    bits.isolateRightmostZeroBit(first);
+    bits.isolateRightmostZeroBit(124);
   }
 }
