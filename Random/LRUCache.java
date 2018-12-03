34b3ed2d2723232186fe5c59b1b6e04623d1a729
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-03 23:49:50 +0530

    LRU Cache implimentation

--- Random/LRUCache.java
+++ Random/LRUCache.java
@@ -27,18 +27,64 @@ public class LRUCache {
 
     if(node == null) return null;
 
-    if(item != head) {
-      //Remove from list
-      //Insert at begin
+    if(node != head) {
+      removeNodeFromLList(node);
+      insertAtBegining(node);
     }
-    return item.value;
+    return node.value;
+  }
+
+  public void print() {
+    System.out.println(map);
   }
 
   private void removeNodeFromLList(Node node) {
-    // Yet to be impliment
+    if(node == null) return;
+    if(node.prev != null) node.prev.next = node.next;
+    if(node.next != null) node.next.prev = node.prev;
+    if(node == tail) tail = node.prev;
+    if(node == head) head = node.next;
+  }
+
+  private void insertAtBegining(Node node) {
+    if(head == null) {
+      head = node;
+      tail = node;
+    } else {
+      head.prev = node;
+      node.next = head;
+      head = node;
+    }
+  }
+
+  public void removeKey(int key) {
+    Node node = map.get(key);
+    removeNodeFromLList(node);
+    map.remove(key);
+  }
+
+  public void setKeyValue(int key, String value) {
+    removeKey(key);
+
+    if (map.size() >= maxCapacity && tail != null) {
+      removeKey(tail.key);
+    }
+
+    Node node = new Node(key, value);
+    insertAtBegining(node);
+    map.put(key, node);
   }
 
   public static void main(String[] args) {
     LRUCache cache = new LRUCache(5);
+    cache.setKeyValue(3, "3");
+    cache.setKeyValue(4, "4");
+    cache.setKeyValue(12, "12");
+    cache.setKeyValue(12, "12");
+    cache.setKeyValue(6, "6");
+    cache.setKeyValue(7, "7");
+    cache.setKeyValue(6, "6");
+    cache.setKeyValue(1, "1");
+    cache.print();
   }
 }
