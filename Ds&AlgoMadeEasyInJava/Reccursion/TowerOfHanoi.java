4b0ce0d601f47b7c8731b45f919485a7511886c2
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-07 13:46:08 +0530

    [Recursion] Towe of hanoi problem

---
+++ Ds&AlgoMadeEasyInJava/Reccursion/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+public class TowerOfHanoi {
+  public static void solve(int rings) {
+    solve(rings, "Source", "Aux", "Dest");
+  }
+
+  private static void solve(int rings, String start, String aux, String end) {
+    if(rings == 1) {
+      System.out.println("Move ring 1 from " + start + " to " + end);
+      return;
+    }
+    solve(rings - 1, start, end, aux);
+    System.out.println("Move ring " + rings + " from " + start + " to " + end);
+    solve(rings - 1, aux, start, end);
+  }
+  public static void main(String[] args) {
+    solve(3);
+  }
+}
