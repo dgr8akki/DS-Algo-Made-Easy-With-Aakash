108f25163ac2a6bf54f2024912fa684fada7effb
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-13 23:46:48 +0530

    Find GCD of two numbers

---
+++ Random/GCD.java
@@ -0,0 +1,22 @@
+public class GCD {
+
+  public int calculateRecursive(int first, int second) {
+    if(first == 0) return second;
+    if(second == 0) return first;
+    return calculateRecursive(second, first % second);
+  }
+
+  public int calculateIterative(int first, int second) {
+    while(second != 0) {
+      int temp = second;
+      second = first % second;
+      first = temp;
+    }
+    return first;
+  }
+  public static void main(String[] args) {
+    GCD gcd = new GCD();
+    System.out.println(gcd.calculateRecursive(40, 100));
+    System.out.println(gcd.calculateIterative(100, 40));
+  }
+}
