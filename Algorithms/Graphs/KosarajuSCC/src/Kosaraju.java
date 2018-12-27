e024bddf500da217e620b1717c3b31f6ea0c943b
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-27 08:40:46 +0530

    Add Kosaraju Strongly connected components problem

---
+++ Algorithms/Graphs/KosarajuSCC/src/Kosaraju.java
@@ -0,0 +1,51 @@
+public class Kosaraju {
+  private int[] id;
+  private boolean[] marked;
+  private int count;
+
+  public Kosaraju(Graph graph) {
+    DepthFirstOrder dfs = new DepthFirstOrder(graph.getTransposedGraph());
+    marked = new boolean[graph.getVertexList().size()];
+    id = new int[graph.getVertexList().size()];
+
+    for (Vertex vertex : dfs.getStack())
+      if (!marked[vertex.getId()]) {
+        dfs(vertex);
+        count++;
+      }
+  }
+
+  private void dfs(Vertex vertex) {
+    marked[vertex.getId()] = true;
+    id[vertex.getId()] = count;
+    vertex.setComponentId(count);
+
+    for (Vertex v : vertex.getAdjacencyList())
+      if (!marked[v.getId()])
+        dfs(v);
+  }
+
+  public int[] getId() {
+    return id;
+  }
+
+  public void setId(int[] id) {
+    this.id = id;
+  }
+
+  public boolean[] getMarked() {
+    return marked;
+  }
+
+  public void setMarked(boolean[] marked) {
+    this.marked = marked;
+  }
+
+  public int getCount() {
+    return count;
+  }
+
+  public void setCount(int count) {
+    this.count = count;
+  }
+}
---
+++ Algorithms/Graphs/KosarajuSCC/src/Vertex.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+  private int id, componentId;
+  private String data;
+  private List<Vertex> adjacencyList;
+  private double distance;
+  private Vertex predecessor;
+  private boolean isVisited;
+
+
+  Vertex(int  id, String data) {
+    this.id = id;
+    this.data = data;
+    this.adjacencyList = new ArrayList<>();
+    this.distance = Double.POSITIVE_INFINITY;
+  }
+
+  Vertex getPredecessor() {
+    return predecessor;
+  }
+
+  void setPredecessor(Vertex predecessor) {
+    this.predecessor = predecessor;
+  }
+
+  public boolean isVisited() {
+    return isVisited;
+  }
+
+  public void setVisited(boolean visited) {
+    isVisited = visited;
+  }
+
+  void addNeighbour(Vertex v) {
+    this.adjacencyList.add(v);
+  }
+
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String data) {
+    this.data = data;
+  }
+
+  List<Vertex> getAdjacencyList() {
+    return adjacencyList;
+  }
+
+  public void setAdjacencyList(List<Vertex> adjacencyList) {
+    this.adjacencyList = adjacencyList;
+  }
+
+  double getDistance() {
+    return distance;
+  }
+
+  void setDistance(double distance) {
+    this.distance = distance;
+  }
+
+  @Override
+  public String toString() {
+    return this.data;
+  }
+
+  public int getComponentId() {
+    return componentId;
+  }
+
+  public void setComponentId(int componentId) {
+    this.componentId = componentId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+}
