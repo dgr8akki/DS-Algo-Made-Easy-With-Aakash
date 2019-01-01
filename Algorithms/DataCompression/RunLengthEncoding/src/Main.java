db1059f59735318c8ef6fa60b95e9d9a99bec8aa
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-01 20:09:42 +0530

    Add run length encoding data compression algorithm

---
+++ Algorithms/DataCompression/RunLengthEncoding/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    String text = "AAAAAAAAABBBBBBCCCCCCCDDDDDDDdaaaaaaaa";
+    RLE rle = new RLE(text);
+    System.out.println("Actual String:");
+    System.out.println(rle.getText());
+    System.out.println("Encoded String:");
+    System.out.println(rle.encode());
+    System.out.println("Decoded String:");
+    String decodedString = rle.decode();
+    System.out.println(decodedString);
+    System.out.println("Is there any loss after decoded?");
+    System.out.println(decodedString.equals(text) ? "No" : "Yes");
+  }
+}
