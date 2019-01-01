7ac40867852ee961ee5ec76e701de8f0065aeed2
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-01 22:14:05 +0530

    Add LZW encoding algorithm

---
+++ Algorithms/DataCompression/LZW/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    LZW lzw = new LZW();
+    List<Integer> compressedString = lzw.compress("DARRARDARDAR");
+    System.out.println(compressedString);
+    String decompressedString = lzw.decompress(compressedString);
+    System.out.println(decompressedString);
+  }
+}
