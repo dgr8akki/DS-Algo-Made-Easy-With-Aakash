3d06e95001e3ae113bacc239a7ceff0220048696
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-07 21:34:07 +0530

    Add chaining hash table

---
+++ Algorithms/HashTable/ChainingHashTable/src/HashItem.java
@@ -0,0 +1,35 @@
+public class HashItem {
+
+  private int key;
+  private int value;
+  private HashItem next;
+
+  public HashItem(int key, int value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public int getKey() {
+    return key;
+  }
+
+  public void setKey(int key) {
+    this.key = key;
+  }
+
+  public HashItem getNext() {
+    return next;
+  }
+
+  public void setNext(HashItem next) {
+    this.next = next;
+  }
+}
