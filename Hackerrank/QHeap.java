62790ca39f6cde510a578077d0c937b138aa93e8
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-12 16:31:28 +0530

    Add Qheap problem

---
+++ Hackerrank/QHeap.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class QHeap {
+
+    public static void main(String[] args) {
+      PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer c1, Integer c2) {
+          return Integer.compare(c1, c2);
+        }
+      });
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      for(int i = 0; i < n; i++) {
+        int query = sc.nextInt();
+        switch(query) {
+          case 1:
+            queue.add(sc.nextInt());
+            break;
+          case 2:
+            queue.remove(sc.nextInt());
+            break;
+          case 3:
+            System.out.println(queue.peek()); break;
+        }
+      }
+    }
+}
+
