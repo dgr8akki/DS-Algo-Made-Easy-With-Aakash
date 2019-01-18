9088683cdb37d9578d3f9dfd7f526337cdae095e
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-18 23:14:07 +0530

    [CodingNinjas] Add basic recurssion problem's solutions

---
+++ CodingNinjas/BasicsOfRecurssion/AllIndexes.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+  public static int[] allIndexes(int input[], int x) {
+    List<Integer> indices = new ArrayList<>();
+    allIndexes(input, indices, x, 0);
+    int[] result = new int[indices.size()];
+    for (int i = 0; i < indices.size(); i++)
+      result[i] = indices.get(i);
+    return result;
+  }
+
+  private static void allIndexes(int[] input, List<Integer> indices, int x, int index) {
+    if (input.length == index)
+      return;
+    if (input[index] == x)
+      indices.add(index);
+    allIndexes(input, indices, x, index + 1);
+  }
+}
---
+++ CodingNinjas/BasicsOfRecurssion/ArraySum.java
@@ -0,0 +1,10 @@
+class Solution {
+  public static int sum(int input[]) {
+    return sum(input, input.length - 1);
+  }
+
+  private static int sum(int input[], int index) {
+    if (index < 0) return 0;
+    return input[index] + sum(input, index - 1);
+  }
+}
