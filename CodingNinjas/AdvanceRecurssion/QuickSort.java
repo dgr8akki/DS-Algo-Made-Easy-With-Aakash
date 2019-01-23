80759bc412da148b7b3e0cf50a67c4bc3891dceb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-01-23 23:21:34 +0530

    [CodingNinjas] Add Advance recurssion problem solutions

---
+++ CodingNinjas/AdvanceRecurssion/QuickSort.java
@@ -0,0 +1,39 @@
+public class QuickSort {
+
+  public static void quickSort(int[] input) {
+    quickSort(input, 0, input.length - 1);
+  }
+
+  private static void quickSort(int[] input, int start, int end) {
+    if (start < end) {
+      int pivot = partition(input, start, end);
+      quickSort(input, start, pivot - 1);
+      quickSort(input, pivot + 1, end);
+    }
+  }
+
+  private static int partition(int[] input, int start, int end) {
+    int count = 0;
+    for (int i = start; i < end; i++)
+      if (input[i] < input[end])
+        count++;
+    int pivotIndex = start + count;
+    if (pivotIndex != end)
+      swap(input, pivotIndex, end);
+    while (start != pivotIndex || end != pivotIndex) {
+      if (input[start] < input[pivotIndex])
+        start++;
+      else if (input[end] >= input[pivotIndex])
+        end--;
+      else
+        swap(input, start, end);
+    }
+    return pivotIndex;
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
---
+++ CodingNinjas/AdvanceRecurssion/ReplaceCharacterRecursively.java
@@ -0,0 +1,21 @@
+public class ReplaceCharacterRecursively {
+
+  /**
+   * Given an input string S and two characters c1 and c2, you need to replace every occurrence of character c1 with character c2 in the given string.
+   * @param input
+   * @param c1
+   * @param c2
+   * @return
+   */
+
+	public static String replaceCharacter(String input, char c1, char c2) {
+		return replaceCharacter(input, 0, c1, c2, "");
+	}
+  	private static String replaceCharacter(String input, int index, char c1, char c2, String res) {
+		if(index == input.length())
+          return res;
+      	if(input.charAt(index) == c1)
+          return replaceCharacter(input, index + 1, c1, c2, res + c2);
+      	return replaceCharacter(input, index + 1, c1, c2, res + input.charAt(index));
+	}
+}
