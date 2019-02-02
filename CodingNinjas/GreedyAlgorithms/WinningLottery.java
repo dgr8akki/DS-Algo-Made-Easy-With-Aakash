fe630ec5f5cac74d4094f787976583164faf0970
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-02 21:47:53 +0530

    [CodingNinjas] Add Winning the lottery problem

---
+++ CodingNinjas/GreedyAlgorithms/WinningLottery.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * WinningLottery
+ */
+public class WinningLottery {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int s = in.nextInt();
+    int d = in.nextInt();
+    in.close();
+    int[] arr = new int[d];
+    arr[0] = 1; s--;
+    int i = d - 1;
+    while(s > 0 && i > 0) {
+      if(s <= 9) {
+        arr[i] = s;
+        break;
+      } else {
+        arr[i] = 9;
+        s -= 9;
+      }
+      i--;
+    }
+
+    for(int a : arr) {
+      System.out.print(a);
+    }
+  }
+}
