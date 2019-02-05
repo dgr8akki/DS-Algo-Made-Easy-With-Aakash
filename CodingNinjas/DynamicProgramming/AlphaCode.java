1fa80c1121b415068f5978bd69d31d3d794d1c4f
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-06 00:20:14 +0530

    [CodingNinjas] Add alphacode recursive with memoization

--- CodingNinjas/DynamicProgramming/AlphaCode.java
+++ CodingNinjas/DynamicProgramming/AlphaCode.java
@@ -17,12 +17,27 @@ public class AlphaCode {
     return (int) (count % M);
   }
 
+  public static int alphaCodeRecursiveMemoized(String s, int[] cache) {
+    long count = 0;
+    if(s.length() == 0 || s.length() == 1)
+      return 1;
+    if(cache[s.length()] > 0) return cache[s.length()];
+    count = alphaCodeRecursive(s.substring(1));
+
+    if((Character.getNumericValue(s.charAt(0)) * 10) + Character.getNumericValue(s.charAt(1)) <= 26)
+      count += alphaCodeRecursive(s.substring(2));
+    cache[s.length()] = (int) (count % M);
+    return cache[s.length()];
+  }
+
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     while(true) {
       String s = in.next();
       if(s.length() == 1 && s.charAt(0) == '0') break;
       System.out.println(alphaCodeRecursive(s));
+      int[] cache = new int[s.length() + 1];
+      System.out.println(alphaCodeRecursiveMemoized(s, cache));
     }
     in.close();
   }
