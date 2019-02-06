808f6e83dc5caae0e3a407aa44ce4a5a123ceeca
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-06 20:08:06 +0530

    [CodingNinjas] Add alphaCode iterative

--- CodingNinjas/DynamicProgramming/AlphaCode.java
+++ CodingNinjas/DynamicProgramming/AlphaCode.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -19,15 +20,33 @@ public class AlphaCode {
 
   public static int alphaCodeRecursiveMemoized(String s, int[] cache) {
     long count = 0;
-    if(s.length() == 0 || s.length() == 1)
-      return 1;
+    if(s.length() != 0 && s.charAt(0) == '0') return 0;
+    // if(s.length() == 0 || s.length() == 1) return 1;
+    cache[0] = 1;
+    cache[1] = 1;
     if(cache[s.length()] > 0) return cache[s.length()];
-    count = alphaCodeRecursive(s.substring(1));
-
+    count = alphaCodeRecursiveMemoized(s.substring(1), cache);
     if((Character.getNumericValue(s.charAt(0)) * 10) + Character.getNumericValue(s.charAt(1)) <= 26)
-      count += alphaCodeRecursive(s.substring(2));
+      count += alphaCodeRecursiveMemoized(s.substring(2), cache);
     cache[s.length()] = (int) (count % M);
     return cache[s.length()];
+  }
+
+  public static int alphaCodeIterative (String s) {
+    int length = s.length();
+    int[] counts = new int[length + 1];
+    counts[0] = 1;
+    counts[1] = 1;
+    for(int i = 2; i <= length; i++) {
+      counts[i] = counts[i - 1];
+      if(s.charAt(i - 1) == '0') {
+        counts[i] = 0;
+      }
+      int alphabetIndex = Character.getNumericValue(s.charAt(i - 2)) * 10 + Character.getNumericValue(s.charAt(i - 1));
+      if(alphabetIndex >= 10 && alphabetIndex <= 26)
+        counts[i] = (counts[i] % M + counts[i - 2] % M) % M;
+    }
+    return counts[length] % M;
   }
 
   public static void main(String[] args) {
@@ -35,9 +54,10 @@ public class AlphaCode {
     while(true) {
       String s = in.next();
       if(s.length() == 1 && s.charAt(0) == '0') break;
-      System.out.println(alphaCodeRecursive(s));
-      int[] cache = new int[s.length() + 1];
-      System.out.println(alphaCodeRecursiveMemoized(s, cache));
+      int[] arr = new int[s.length() + 1];
+      System.out.println(alphaCodeRecursiveMemoized(s, arr));
+      System.out.println(Arrays.toString(arr));
+      System.out.println(alphaCodeIterative(s));
     }
     in.close();
   }
