00807b4d46067c8eff415a36e55600826d22ce93
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-16 14:52:33 +0530

    [CodingNinjas] Add adjacentBitCount memoized solution

--- CodingNinjas/DynamicProgramming/AdjacentBitCounts.java
+++ CodingNinjas/DynamicProgramming/AdjacentBitCounts.java
@@ -19,11 +19,32 @@ public class AdjacentBitCounts {
   }
 
   private static long solve(int n, int k) {
-    long result = bitCountUtil(n, k, false) + bitCountUtil(n, k, true);
-    return result;
+    // long recursiveResult = bitCountRecursiveUtil(n, k, false) + bitCountRecursiveUtil(n, k, true);
+    long[][][] dp = new long[n + 1][k + 1][2];
+    long memoizedResult = bitCountMemoizedUtil(n, k, false, dp) + bitCountMemoizedUtil(n, k, true, dp);
+    return memoizedResult;
   }
 
-  private static long bitCountUtil(int n, int k, boolean isFirstBitSet) {
+  private static long bitCountMemoizedUtil(int n, int k, boolean isFirstBitSet, long[][][] dp) {
+    if(k < 0)
+      return 0;
+    if(n == 1) {
+      if(k == 0) return 1;
+      return 0;
+    }
+    dp[1][0][0] = 1;
+    dp[1][0][1] = 1;
+    if(isFirstBitSet) {
+      if(dp[n][k][1] <= 0)
+        dp[n][k][1] = bitCountMemoizedUtil(n - 1, k - 1, true, dp) + bitCountMemoizedUtil(n - 1, k, false, dp);
+      return dp[n][k][1];
+    }
+    if(dp[n][k][0] <= 0)
+      dp[n][k][0] = bitCountMemoizedUtil(n - 1, k, true, dp) + bitCountMemoizedUtil(n - 1, k, false, dp);
+    return dp[n][k][0];
+  }
+
+  private static long bitCountRecursiveUtil(int n, int k, boolean isFirstBitSet) {
     if(k < 0)
       return 0;
     if(n == 1) {
@@ -31,7 +52,7 @@ public class AdjacentBitCounts {
       return 0;
     }
     if(isFirstBitSet)
-      return bitCountUtil(n - 1, k - 1, true) + bitCountUtil(n - 1, k, false);
-    return bitCountUtil(n - 1, k, true) + bitCountUtil(n - 1, k, false);
+      return bitCountRecursiveUtil(n - 1, k - 1, true) + bitCountRecursiveUtil(n - 1, k, false);
+    return bitCountRecursiveUtil(n - 1, k, true) + bitCountRecursiveUtil(n - 1, k, false);
   }
 }
