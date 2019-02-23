f36e3ecb859dc1f2af01d37da57a700f3c7bf77e
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-23 19:55:02 +0530

    [CodingNinjas][DynamicProgrammin2] Add LCS iterative

--- CodingNinjas/DynamicProgramming2/LCS.java
+++ CodingNinjas/DynamicProgramming2/LCS.java
@@ -4,8 +4,9 @@
 public class LCS {
   public static int lcs(String s1, String s2) {
     // return lcsRecursive(s1, s2);
-    int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-    return lcsMemoized(s1, s2, 0, 0, dp);
+    // int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+    // return lcsMemoized(s1, s2, 0, 0, dp);
+    return lcsIterative(s1, s2);
 	}
 
   private static int lcsRecursive(String s1, String s2) {
@@ -26,6 +27,16 @@ public class LCS {
     }
     dp[s1Index][s2Index] = Math.max(lcsMemoized(s1,s2,s1Index + 1, s2Index, dp), lcsMemoized(s1, s2, s1Index, s2Index + 1, dp));
     return dp[s1Index][s2Index];
+  }
+
+  private static int lcsIterative(String s1, String s2) {
+    if(s1.length() == 0 || s2.length() == 0) return 0;
+    int s1Size = s1.length(), s2Size = s2.length();
+    int[][] dp = new int[s1Size + 1][s2Size + 1];
+    for(int i = 1; i <= s1Size; i++)
+      for(int j = 1; j <= s2Size; j++)
+        dp[i][j] = s1.charAt(i - 1) == s2.charAt(j - 1) ? 1 + dp[i - 1][j - 1] : Math.max(dp[i - 1][j], dp[i][j - 1]);
+    return dp[s1Size][s2Size];
   }
 
   public static void main(String[] args) {
