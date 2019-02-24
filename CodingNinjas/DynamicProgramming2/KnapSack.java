9d9897cd94d5f2e713311167fcb143de34a9a0b1
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-02-24 14:42:54 +0530

    [CodingNinjas][DynamicProgrammin2] Add Knapsack memoized solution

--- CodingNinjas/DynamicProgramming2/KnapSack.java
+++ CodingNinjas/DynamicProgramming2/KnapSack.java
@@ -3,21 +3,43 @@
  */
 public class KnapSack {
 
-  public static int knapsack(int[] weight,int value[],int maxWeight) {
-    return knapsackRecursive(weight, value, maxWeight, 0);
-	}
+  public static int knapsack(int[] weight, int value[], int maxWeight) {
+    // return knapsackRecursive(weight, value, maxWeight, 0);
+    int[][] dp = new int[weight.length + 1][maxWeight + 1];
+    return knapsackMemoized(weight, value, maxWeight, 0, dp);
+  }
 
-  private static int knapsackRecursive(int[] weight,int value[],int maxWeight, int currentItemIdx) {
-    if(currentItemIdx >= value.length) return 0;
+  private static int knapsackRecursive(int[] weight, int value[], int maxWeight, int currentItemIdx) {
+    if (currentItemIdx >= value.length)
+      return 0;
     int option1 = knapsackRecursive(weight, value, maxWeight, currentItemIdx + 1);
     int option2 = Integer.MIN_VALUE;
-    if(maxWeight - weight[currentItemIdx] >= 0)
-      option2 = value[currentItemIdx] + knapsackRecursive(weight, value, maxWeight - weight[currentItemIdx], currentItemIdx + 1);
+    if (maxWeight - weight[currentItemIdx] >= 0)
+      option2 = value[currentItemIdx]
+          + knapsackRecursive(weight, value, maxWeight - weight[currentItemIdx], currentItemIdx + 1);
     return Math.max(option1, option2);
-	}
+  }
+
+  private static int knapsackMemoized(int[] weight, int value[], int maxWeight, int currentItemIdx, int[][] dp) {
+    if (currentItemIdx >= value.length || maxWeight == 0)
+      return 0;
+    if (dp[currentItemIdx][maxWeight] > 0)
+      return dp[currentItemIdx][maxWeight];
+    int option1 = knapsackMemoized(weight, value, maxWeight, currentItemIdx + 1, dp);
+    if (maxWeight - weight[currentItemIdx] >= 0) {
+      int option2 = value[currentItemIdx]
+          + knapsackMemoized(weight, value, maxWeight - weight[currentItemIdx], currentItemIdx + 1, dp);
+      dp[currentItemIdx][maxWeight] = Math.max(option1, option2);
+    } else {
+      dp[currentItemIdx][maxWeight] = option1;
+    }
+
+    return dp[currentItemIdx][maxWeight];
+  }
+
   public static void main(String[] args) {
-    int[] w = {1, 2, 4, 5};
-    int[] val = {5, 4, 8, 6};
+    int[] w = { 1, 2, 4, 5 };
+    int[] val = { 5, 4, 8, 6 };
     int cap = 5;
     System.out.println(knapsack(w, val, cap));
   }
