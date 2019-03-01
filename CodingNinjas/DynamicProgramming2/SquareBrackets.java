53a71d48fd038452d79934a3a2f067727fbe7daf
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-03-02 00:10:14 +0530

    [CodingNinjas][DynamicProgramming2] Add square bracket memoized problem

--- CodingNinjas/DynamicProgramming2/SquareBrackets.java
+++ CodingNinjas/DynamicProgramming2/SquareBrackets.java
@@ -20,7 +20,28 @@ public class SquareBrackets {
   }
 
   private static int solve(int n, boolean[] isOnlyOpenNeededHere) {
-    return squareBracketUtil(n, isOnlyOpenNeededHere, 0, 0);
+    int[][] dp = new int[n + 1][n + 1];
+    return squareBracketMemoizedUtil(n, isOnlyOpenNeededHere, 0, 0, dp);
+  }
+
+  private static int squareBracketMemoizedUtil(int n, boolean[] isOnlyOpenNeededHere, int currentOpeningCount, int currentClosingCount, int[][] dp) {
+    if(currentClosingCount > n || currentOpeningCount > n) return 0;
+    if(currentClosingCount == n && currentOpeningCount == n) return 1;
+    if(dp[currentOpeningCount][currentClosingCount] > 0) return dp[currentOpeningCount][currentClosingCount];
+    //Only opening bracket.
+    if(currentClosingCount == currentOpeningCount || isOnlyOpenNeededHere[currentClosingCount + currentOpeningCount]) {
+      dp[currentOpeningCount][currentClosingCount] = squareBracketMemoizedUtil(n, isOnlyOpenNeededHere, currentOpeningCount + 1, currentClosingCount, dp);
+      return dp[currentOpeningCount][currentClosingCount];
+    }
+    //Only closing bracket.
+    if(currentOpeningCount == n) {
+      dp[currentOpeningCount][currentClosingCount] = squareBracketMemoizedUtil(n, isOnlyOpenNeededHere, currentOpeningCount, currentClosingCount + 1, dp);
+      return dp[currentOpeningCount][currentClosingCount];
+    }
+    //Both can be apply
+    dp[currentOpeningCount][currentClosingCount] = squareBracketMemoizedUtil(n, isOnlyOpenNeededHere, currentOpeningCount, currentClosingCount + 1, dp)
+    + squareBracketMemoizedUtil(n, isOnlyOpenNeededHere, currentOpeningCount + 1, currentClosingCount, dp);
+    return dp[currentOpeningCount][currentClosingCount];
   }
 
   private static int squareBracketUtil(int n, boolean[] isOnlyOpenNeededHere, int currentOpeningCount, int currentClosingCount) {
