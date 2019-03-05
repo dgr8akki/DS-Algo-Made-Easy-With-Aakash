08928089abe77787c67327f70d6f76daee3112e0
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-03-05 22:34:38 +0530

    [CodingNinjas][SegmentTree] Fix Two vs Three bug

--- CodingNinjas/SegmentTree/TwoVsThree.java
+++ CodingNinjas/SegmentTree/TwoVsThree.java
@@ -9,26 +9,28 @@ public class TwoVsThree {
   String input;
   int[] cache;
   private int[] tree;
+  int[] pow;
 
   TwoVsThree(String s) {
     this.input = s;
     this.cache = new int[this.input.length()];
     this.tree = new int[4 * this.input.length()];
+    this.pow = new int[this.input.length() + 1];
+    pow[0] = 1;
+    for(int i = 1; i <= this.input.length(); ++i)
+        pow[i] = (pow[i-1]*2)%3;
     this.buildTree(0, this.input.length() - 1, 1);
   }
 
   private void buildTree(int start, int end, int treeNode) {
-    if(start == end) {
+    if (start == end) {
       tree[treeNode] = input.charAt(start) == '0' ? 0 : 1;
       return;
     }
     int mid = (start + end) / 2;
     buildTree(start, mid, 2 * treeNode);
     buildTree(mid + 1, end, (2 * treeNode) + 1);
-    int right = tree[(2 * treeNode) + 1];
-    int left = tree[2 * treeNode];
-    int ans = (right + (((int)Math.pow(2, end - mid) % 3 * left) %3) %3) %3;
-    tree[treeNode] = ans;
+    tree[treeNode] = ((tree[2 * treeNode] * pow[end - mid]) + tree[(2 * treeNode) + 1]) % 3;
   }
 
   public void print() {
@@ -44,35 +46,33 @@ public class TwoVsThree {
   }
 
   private int query(int start, int end, int treeNode, int startingIndex, int endIndex) {
-    //Fully outside
-    if(startingIndex > end || endIndex < start) return 0;
-    //Fully overlap
-    if(startingIndex <= start && endIndex >= end) return tree[treeNode] % 3;
-    //Partially overlap
+    // Fully outside
+    if (start > end || startingIndex > end || endIndex < start)
+      return 0;
+    // Fully overlap
+    if (startingIndex <= start && endIndex >= end)
+      return (pow[endIndex - end] * tree[treeNode]) % 3;
+    // Partially overlap
     int mid = (start + end) / 2;
     int left = query(start, mid, 2 * treeNode, startingIndex, endIndex);
     int right = query(mid + 1, end, (2 * treeNode) + 1, startingIndex, endIndex);
-    int ans = (right + ((int)(Math.pow(2, endIndex - end) % 3 * left) %3) %3) %3;
-    System.out.println("L: " + left + ", R: " + right + ", A:" + ans);
-    return ans;
+    return (left + right) % 3;
   }
 
   public void update(int start, int end, int treeNode, int index) {
-    if(start == end  && start == index) {
-      if(input.charAt(index) == '1' || cache[index] == 1) return;
+    if (start == end && start == index) {
+      if (input.charAt(index) == '1' || cache[index] == 1)
+        return;
       tree[treeNode] = 1;
       cache[index] = 1;
       return;
     }
     int mid = (start + end) / 2;
-    if(index > mid)
+    if (index > mid)
       update(mid + 1, end, (2 * treeNode) + 1, index);
     else
       update(start, mid, 2 * treeNode, index);
-    int right = tree[(2 * treeNode) + 1];
-    int left = tree[2 * treeNode];
-    int ans = (right + (((int)Math.pow(2, end - mid) % 3 * left) %3) %3) %3;
-    tree[treeNode] = ans;
+    tree[treeNode] = ((tree[2 * treeNode] * pow[end - mid]) + tree[(2 * treeNode) + 1]) % 3;
   }
 
   public static void main(String[] args) {
@@ -80,7 +80,6 @@ public class TwoVsThree {
     in.nextInt();
     String input = in.next();
     TwoVsThree segTree = new TwoVsThree(input);
-    segTree.print();
     int q = in.nextInt();
     while (q-- > 0) {
       int type = in.nextInt();
@@ -92,7 +91,6 @@ public class TwoVsThree {
         break;
       case 1:
         segTree.update(in.nextInt());
-        segTree.print();
         break;
       }
     }
