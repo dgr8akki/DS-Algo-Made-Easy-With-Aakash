f87755a5312e5756972171437462df9403f27130
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-03-11 08:51:04 +0530

    [CodingNinjas][Graphs - I] Fix connecting dots problem

--- CodingNinjas/Graphs - I/ConnectingDots.java
+++ CodingNinjas/Graphs - I/ConnectingDots.java
@@ -4,8 +4,6 @@
 public class ConnectingDots {
 
   public int solve(String[] Graph, int N, int M) {
-    // int[] xOffset = { 0, 1, 1, -1, 0, -1, -1, 1 };
-    // int[] yOffset = { -1, -1, 0, -1, 1, 0, 1, 1 };
     int[] xOffset = { 0, 1, 0, -1 };
     int[] yOffset = { -1, 0, 1, 0 };
     boolean[][] isVisited = new boolean[N][M];
@@ -18,47 +16,33 @@ public class ConnectingDots {
             dfsUtil(i, j, isVisited, isBeingVisited, xOffset, yOffset, N, M, current, Graph, c);
             if (current[0] > 4)
               return 1;
+            current[0] = 0;
           }
         }
       }
-      current[0] = 0;
     }
     return 0;
   }
 
-  private void dfsUtil(int x, int y, boolean[][] isVisited, boolean[][] isBeingVisited, int[] xOffset, int[] yOffset, int N, int M, int[] count,
-      String cake[], char currentCharacter) {
-    // if (count[0] >= ) {
-    // return;
-    // }
+  private void dfsUtil(int x, int y, boolean[][] isVisited, boolean[][] isBeingVisited, int[] xOffset, int[] yOffset,
+      int N, int M, int[] count, String cake[], char currentCharacter) {
     isBeingVisited[x][y] = true;
     count[0]++;
-    // isVisited[x][y] = true;
     for (int i = 0; i < xOffset.length; i++) {
       if (isValid(x + xOffset[i], y + yOffset[i], isVisited, isBeingVisited, N, M, cake, currentCharacter)) {
         isVisited[x + xOffset[i]][y + yOffset[i]] = true;
         dfsUtil(x + xOffset[i], y + yOffset[i], isVisited, isBeingVisited, xOffset, yOffset, N, M, count, cake,
             currentCharacter);
-      if(isBeingVisited[x + xOffset[i]][y + yOffset[i]] && count[0] >= 4) break;
+        if (isBeingVisited[x + xOffset[i]][y + yOffset[i]] && count[0] >= 3)
+          break;
       }
-    isVisited[x][y] = true;
-    isBeingVisited[x][y] = true;
+      isVisited[x][y] = true;
+      isBeingVisited[x][y] = true;
     }
   }
 
-  private boolean isValid(int x, int y, boolean[][] isVisited, boolean[][] isBeingVisited, int N, int M, String cake[], char currentCharacter) {
-    return x >= 0 && x < N && y < M && y >= 0 && cake[x].charAt(y) == currentCharacter && !isBeingVisited[x][y] && !isVisited[x][y];
-  }
-
-  public static void main(String[] args) {
-    ConnectingDots dots = new ConnectingDots();
-    int N = 3;
-    int M = 4;
-    String[] Graph = new String[N];
-    Graph[0] = "AAAA";
-    Graph[1] = "ABCA";
-    Graph[2] = "AAAA";
-
-    System.out.println(dots.solve(Graph, N, M));
+  private boolean isValid(int x, int y, boolean[][] isVisited, boolean[][] isBeingVisited, int N, int M, String cake[],
+      char currentCharacter) {
+    return x >= 0 && x < N && y < M && y >= 0 && cake[x].charAt(y) == currentCharacter && !isVisited[x][y];
   }
 }
