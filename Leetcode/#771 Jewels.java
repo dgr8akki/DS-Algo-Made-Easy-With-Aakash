d89993fdb649bb9b03b81802df3c7946cd15e53d
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-03-17 15:30:30 +0530

    [LeetCode][#771] Jewels problem

---
+++ Leetcode/#771 Jewels.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int[] characterSet = new int[128];
+        int count = 0;
+        for(char c : S.toCharArray()) {
+            System.out.println((int)c);
+            characterSet[(int)c]++;
+        }
+        for(char c : J.toCharArray())
+            count+=characterSet[(int)c];
+        System.out.println(Arrays.toString(characterSet));
+        return count;
+    }
+
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    System.out.print(sol.numJewelsInStones("aA", "aAAbbb"));
+  }
+}
