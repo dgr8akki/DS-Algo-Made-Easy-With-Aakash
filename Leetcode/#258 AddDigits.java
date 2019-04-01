3160eadc0c3dab4096bf3657fdf55fd2cf36a083
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-01 23:49:08 +0530

    [LeetCode][#258] Add Digits in constant time

--- Leetcode/#258 AddDigits.java
+++ Leetcode/#258 AddDigits.java
@@ -10,4 +10,16 @@ class Solution {
     }
     return addDigits(sum);
   }
+
+  public int addDigits(int num) {
+    if (num == 0) {
+      return 0;
+    }
+    if (num % 9 == 0) {
+      return 9;
+    } else {
+      return num % 9;
+    }
+  }
+
 }
