c9be6fa00847b975bdd462e0303db03c004be850
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-02 23:39:56 +0530

    [LeetCode][#171] Add ExcelSheetColumn problem

---
+++ Leetcode/#171 ExcelShhetColumn.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int titleToNumber(String s) {
+      int result = 0;
+      for(int i = 0; i < s.length(); i++) {
+        result *=  26;
+        result += (s.charAt(i) - 'A' + 1);
+      }
+        return result;
+    }
+}
