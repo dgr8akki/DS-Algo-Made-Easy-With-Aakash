1d927cd958e1302534a49f6a5ea3d26101fe0f3b
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-03 22:05:53 +0530

    [LeetCode][#326] Add Valid Perfect Square problem

---
+++ Leetcode/#367 ValidPerfectSquare.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long x = num;
+        while (x * x > num) {
+            x = (x + num / x) >> 1;
+        }
+        return x * x == num;
+    }
+}
