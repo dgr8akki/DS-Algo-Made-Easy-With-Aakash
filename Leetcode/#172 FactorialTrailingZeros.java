ae55a77c670b40662a9a4ca50ba7308d1392f086
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-03 22:01:43 +0530

    [LeetCode][#172] Add Factorial Trailing zero problem

---
+++ Leetcode/#172 FactorialTrailingZeros.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while(n > 0) {
+          count += (n / 5);
+          n /= 5;
+        }
+        return count;
+    }
+}
