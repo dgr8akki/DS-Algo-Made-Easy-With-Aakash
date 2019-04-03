2a3f8e37f21aae73ad6c330944949cdab1b1f39e
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-03 22:05:14 +0530

    [LeetCode][#326] Add Is Power Of three problem

---
+++ Leetcode/#326 IsPowerOfThree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0) return false;
+        // while(n % 3 == 0)
+        //   n/=3;
+        // return n == 1;
+        // 1162261467 is 3^19,  3^20 is bigger than int
+      return 1162261467 % n == 0;
+    }
+}
