0859254c83287639ef0cbc282c0bddd99390ff48
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-04 23:44:30 +0530

    [LeetCode][#20] Add Valid paranthesis

---
+++ Leetcode/#20 ValidParanthesis.java
@@ -0,0 +1,36 @@
+class Solution {
+  public boolean isValid(String s) {
+    Stack<Character> st = new Stack<>();
+    if (s.length() == 0)
+      return true;
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      switch (c) {
+      case '(':
+      case '{':
+      case '[':
+        st.push(c);
+        break;
+      case ')':
+        if (!st.isEmpty() && st.peek() == '(') {
+          st.pop();
+          break;
+        } else
+          return false;
+      case '}':
+        if (!st.isEmpty() && st.peek() == '{') {
+          st.pop();
+          break;
+        } else
+          return false;
+      case ']':
+        if (!st.isEmpty() && st.peek() == '[') {
+          st.pop();
+          break;
+        } else
+          return false;
+      }
+    }
+    return st.isEmpty();
+  }
+}
