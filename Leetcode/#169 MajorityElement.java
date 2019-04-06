1786f2c117fc719a79c43fba54da79caec67bd20
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-06 14:08:15 +0530

    [LeetCode][#169] Add Majority Element problem

---
+++ Leetcode/#169 MajorityElement.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int majorityElement(int[] nums) {
+    int maj = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (count == 0) {
+        maj = nums[i];
+        count++;
+      } else if (nums[i] == maj) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+
+    return maj;
+  }
+}
