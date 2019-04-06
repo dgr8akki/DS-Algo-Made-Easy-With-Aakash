a8a659c65134536c2a9a0481af4b6c5c7f255cff
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-06 14:15:11 +0530

    [LeetCode][#896] Add IsMonotonic problem

---
+++ Leetcode/#896 IsMonotonic.java
@@ -0,0 +1,29 @@
+class Solution {
+  public boolean isMonotonic(int[] A) {
+    // System.out.println("isMonotonicIncreasing: " + isMonotonicIncreasing(A));
+    // System.out.println("isMonotonicDecreasing: " + isMonotonicIncreasing(A));
+    return isMonotonicIncreasing(A) || isMonotonicDecreasing(A);
+  }
+
+  public boolean isMonotonicIncreasing(int[] A) {
+    int last = A[0];
+    for (int a : A) {
+      if (a >= last)
+        last = a;
+      else
+        return false;
+    }
+    return true;
+  }
+
+  public boolean isMonotonicDecreasing(int[] A) {
+    int last = A[0];
+    for (int a : A) {
+      if (a <= last)
+        last = a;
+      else
+        return false;
+    }
+    return true;
+  }
+}
