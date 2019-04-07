9736b305c7f561f1d5e5bfb76c78d473fd1f906f
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-07 23:49:20 +0530

    [LeetCode][#225] Stack Using Queue

---
+++ Leetcode/#225 StackUsinQueue.java
@@ -0,0 +1,28 @@
+class MyStack {
+  Queue<Integer> q = new LinkedList<Integer>();
+
+  // Push element x onto stack.
+  public void push(int x) {
+    q.add(x);
+
+    int n = q.size();
+    while (n-- > 1)
+      q.add(q.poll());
+  }
+
+  // Removes the element on top of the stack.
+  public int pop() {
+    return q.poll();
+  }
+
+  // Get the top element.
+  public int top() {
+    return q.peek();
+  }
+
+  // Return whether the stack is empty.
+  public boolean empty() {
+    return q.isEmpty();
+  }
+
+}
