7950465f021ecfd6d26f0e67a36eae2f897f0dd3
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-09 22:51:31 +0530

    [LeetCode][#671] Add Second Minimum node of tree

---
+++ Leetcode/#671 SecondMinimumNode.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        getNodes(root, list);
+        if(list.size() <= 1) return -1;
+        Collections.sort(list);
+        int smallest = list.get(0);
+        int secondSmallest = -1;
+        for(int node: list) {          
+          if(node == smallest) continue;
+          if(node > smallest) return node;
+        }
+      return secondSmallest;
+    }
+  
+  private void getNodes(TreeNode root, List<Integer> list) {
+    if(root == null) return;
+    getNodes(root.left, list);
+    list.add(root.val);
+    getNodes(root.right, list);
+  }
+}
