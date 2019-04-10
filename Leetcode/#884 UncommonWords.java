b3ec17264f0a749722b2e2eed8dd0392561f6676
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-10 22:00:33 +0530

    [LeetCode][#706] Add uncommon words problem

---
+++ Leetcode/#884 UncommonWords.java
@@ -0,0 +1,22 @@
+class Solution {
+  public String[] uncommonFromSentences(String A, String B) {
+    List<String> list = new ArrayList<>();
+    String[] aWords = A.split(" ");
+    String[] bWords = B.split(" ");
+    Map<String, Integer> map = new HashMap<>();
+    for (String word : aWords) {
+      map.put(word, map.getOrDefault(word, 0) + 1);
+    }
+    for (String word : bWords) {
+      map.put(word, map.getOrDefault(word, 0) + 1);
+    }
+
+    for (String word : map.keySet()) {
+      if (map.get(word) == 1)
+        list.add(word);
+    }
+    String[] stockArr = new String[list.size()];
+    stockArr = list.toArray(stockArr);
+    return stockArr;
+  }
+}
