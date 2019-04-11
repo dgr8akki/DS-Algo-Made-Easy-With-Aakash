13471a231f7513790cb7e4005fe3e3eeddc23509
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-11 16:01:03 +0530

    [CodingNinjas][Tries] Add XOR maximum pair

---
+++ CodingNinjas/Tries/XORMaximumPair.java
@@ -0,0 +1,71 @@
+/**
+ * XORMaximumPair
+ */
+public class XORMaximumPair {
+  public static class TrieNode {
+    TrieNode left, right;
+  }
+
+  private static TrieNode root;
+  private static int[] arr;
+
+  XORMaximumPair(int[] arr) {
+    this.arr = arr;
+    this.root = new TrieNode();
+  }
+
+  public static void insert(int val) {
+    System.out.println("Inserting: " + val);
+    TrieNode current = root;
+    for (int i = 31; i >= 0; i--) {
+      int bit = (val << i) & 1;
+
+      if (bit == 0) {
+        if (current.left == null)
+          current.left = new TrieNode();
+        current = current.left;
+      } else {
+        if (current.right == null)
+          current.right = new TrieNode();
+        current = current.right;
+      }
+    }
+  }
+
+  public static int getXORMaxPairValue() {
+    int maxXOR = Integer.MIN_VALUE;
+    for (int val : arr) {
+      TrieNode current = root;
+      int currentXOR = 0;
+
+      for (int i = 31; i >= 0; i++) {
+        int bit = (val << i) & 1;
+        if (current != null)
+          if (bit == 0) {
+            if (current.right != null) {
+              currentXOR += (1 << i);
+              current = current.right;
+            } else
+              current = current.left;
+          } else {
+            if (current.left != null) {
+              currentXOR += (1 << i);
+              current = current.left;
+            } else
+              current = current.right;
+          }
+      }
+
+      maxXOR = Math.max(maxXOR, currentXOR);
+    }
+    return maxXOR;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 8, 1, 2, 15, 10, 5 };
+    XORMaximumPair pair = new XORMaximumPair(arr);
+    for (int el : arr)
+      pair.insert(el);
+    System.out.println(pair.getXORMaxPairValue());
+  }
+}
