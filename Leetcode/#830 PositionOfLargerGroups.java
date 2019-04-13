2c5c607c591fb059df09a6acf58911846ab0b3cb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-13 22:09:47 +0530

    [LeetCode][#830] Position of larger group

---
+++ Leetcode/#830 PositionOfLargerGroups.java
@@ -0,0 +1,26 @@
+class Solution {
+  public List<List<Integer>> largeGroupPositions(String S) {
+    S += "0";
+    List<List<Integer>> res = new ArrayList<>();
+    char last = S.charAt(0);
+    int count = 1;
+    for (int i = 1; i < S.length(); i++) {
+      char current = S.charAt(i);
+      if (current == last) {
+        count++;
+      } else {
+        if (count >= 3) {
+          List<Integer> temp = new ArrayList<>(2);
+          temp.add(i - count);
+          temp.add(i - 1);
+          res.add(temp);
+        }
+
+        count = 1;
+        last = current;
+      }
+    }
+
+    return res;
+  }
+}
