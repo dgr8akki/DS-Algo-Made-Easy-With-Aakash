8b7137df3a4b801a867436d69083e83af63f0eb7
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-13 22:10:27 +0530

    [LeetCode][#985] Sum of even numbers

---
+++ Leetcode/#985 SumOfEvenNumbers.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+    int[] result = new int[queries.length];
+    int sum = 0;
+    for (int i : A) {
+      if (i % 2 == 0)
+        sum += i;
+    }
+    for (int i = 0; i < queries.length; i++) {
+      int index = queries[i][1], prev = A[index];
+      if (prev % 2 == 0)
+        sum -= prev;
+      int num = A[index] += queries[i][0];
+      if (num % 2 == 0)
+        sum += num;
+      result[i] = sum;
+    }
+    return result;
+  }
+}
