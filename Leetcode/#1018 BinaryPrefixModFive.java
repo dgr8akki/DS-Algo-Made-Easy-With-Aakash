db12910120fc6c6e7d28899a6b56bb1629bf774d
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-13 22:03:32 +0530

    [LeetCode][#1018] Binary prefix mode five

---
+++ Leetcode/#1018 BinaryPrefixModFive.java
@@ -0,0 +1,14 @@
+class Solution {
+  public List<Boolean> prefixesDivBy5(int[] A) {
+    List<Boolean> res = new ArrayList<>();
+    int num = 0;
+    for (int i : A) {
+      num = num << 1;
+      if (i == 1)
+        num ^= 1;
+      res.add(num % 5 == 0);
+      num %= 5;
+    }
+    return res;
+  }
+}
