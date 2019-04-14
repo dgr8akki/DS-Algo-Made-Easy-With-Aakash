dff9c1a27ad73cc334295fda317ff651df471e99
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-14 19:24:40 +0530

    [Leetcode][#747] Largest number twice others

---
+++ Leetcode/#747 LargestNumberTwiceOthers.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int dominantIndex(int[] nums) {
+    int firstLargest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+    int idx = 0, res = -1;
+    for (int i : nums) {
+      if (i > firstLargest) {
+        secondLargest = firstLargest;
+        firstLargest = i;
+        res = idx;
+      } else if (i > secondLargest) {
+        secondLargest = i;
+      }
+
+      idx++;
+    }
+
+    return firstLargest >= 2 * secondLargest ? res : -1;
+  }
+}
