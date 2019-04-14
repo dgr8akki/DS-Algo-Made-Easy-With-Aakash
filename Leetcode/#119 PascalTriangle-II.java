e7ff40718694cc924d091b35dc904f8ff3947aed
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-14 19:21:12 +0530

    [Leetcode][#119] Pascal's Triangle - II

---
+++ Leetcode/#119 PascalTriangle-II.java
@@ -0,0 +1,24 @@
+class Solution {
+  public List<Integer> getRow(int numRows) {
+    numRows++;
+    List<List<Integer>> res = new ArrayList<>();
+    if (numRows == 0)
+      return new ArrayList<>();
+    List<Integer> row = new ArrayList<>();
+    row.add(1);
+    res.add(row);
+    for (int i = 2; i <= numRows; i++) {
+      List<Integer> temp = new ArrayList<>();
+      for (int j = 0; j < i; j++) {
+        if (j == 0 || j == i - 1)
+          temp.add(1);
+        else {
+          List<Integer> prev = res.get(i - 2);
+          temp.add(prev.get(j - 1) + prev.get(j));
+        }
+      }
+      res.add(temp);
+    }
+    return res.get(numRows - 1);
+  }
+}
