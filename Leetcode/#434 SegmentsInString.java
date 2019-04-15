611b47932c6353bc0fb068a01557da065013ef6a
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-15 22:36:43 +0530

    [LeetCode][#387] Segments in string

---
+++ Leetcode/#434 SegmentsInString.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int countSegments(String s) {
+    String[] sar = s.split(" ");
+    int count = 0;
+    for (String word : sar) {
+      if (word.length() != 0)
+        count++;
+    }
+    return count;
+  }
+}
