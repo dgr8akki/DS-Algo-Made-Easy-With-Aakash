70af1b05256950afd8e2ffea48fb06e4aafd7fe0
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-16 21:44:10 +0530

    [LeetCode][#849] Maximum Distance

---
+++ Leetcode/#849 MaximumDistance.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int maxDistToClosest(int[] seats) {
+    int i, j, res = 0, n = seats.length;
+    for (i = j = 0; j < n; ++j)
+      if (seats[j] == 1) {
+        if (i == 0)
+          res = j;
+        else
+          res = Math.max(res, (j - i + 1) / 2);
+        i = j + 1;
+      }
+    res = Math.max(res, n - i);
+    return res;
+  }
+}
