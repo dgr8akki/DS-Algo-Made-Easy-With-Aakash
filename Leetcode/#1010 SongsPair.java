73ee81fcdfbbaaf7bb2246db179a21bfb61e31bb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-16 21:42:23 +0530

    [LeetCode][#1010] Songs Pair

---
+++ Leetcode/#1010 SongsPair.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int numPairsDivisibleBy60(int[] time) {
+    int temp[] = new int[60]
+    int count = 0;
+    for (int t : time) {
+      count += temp[(60 - t % 60) % 60];
+      temp[t % 60] += 1;
+    }
+    return count;
+  }
+}
