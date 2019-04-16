7c98499f6dfdc693e9f77d3c1c43ea7e84622fef
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-16 21:41:42 +0530

    [LeetCode][#704] Refactor #704

--- Leetcode/#704 BinarySearch.java
+++ Leetcode/#704 BinarySearch.java
@@ -1,6 +1,15 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int res = Arrays.binarySearch(nums, target);
-        return res >= 0 ? res : -1;
+  public int search(int[] nums, int target) {
+    int start = 0, end = nums.length - 1, mid = -1;
+    while (start <= end) {
+      mid = (start + end) >> 1;
+      if (target == nums[mid])
+        return mid;
+      if (target < nums[mid])
+        end = mid - 1;
+      else
+        start = mid + 1;
     }
+    return -1;
+  }
 }
