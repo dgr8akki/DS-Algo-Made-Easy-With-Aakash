799d1c4a1d3ed7b214434caf04dfef628ba9d94c
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-17 20:28:27 +0530

    [LeetCode][#160] Intersection of linked list

---
+++ Leetcode/#160 IntersectionOfLinkedList.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    if (headA == null || headB == null)
+      return null;
+
+    ListNode tempA = headA;
+    ListNode tempB = headB;
+    while (tempA != tempB) {
+      tempA = tempA == null ? headB : tempA.next;
+      tempB = tempB == null ? headA : tempB.next;
+
+    }
+    return tempB;
+  }
+}
