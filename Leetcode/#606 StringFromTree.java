9a9773eaabbea72455b08a4d8b4ac2f13ba1f340
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-20 17:38:49 +0530

    [LeetCode][#606] String of tree

---
+++ Leetcode/#606 StringFromTree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+  public String tree2str(TreeNode root) {
+    if (root == null)
+      return "";
+    StringBuilder sb = new StringBuilder();
+    tree2str(root, sb);
+    return sb.toString();
+  }
+
+  public void tree2str(TreeNode t, StringBuilder sb) {
+    sb.append(t.val);
+    if (t.left != null) {
+      sb.append('(');
+      tree2str(t.left, sb);
+      sb.append(')');
+    }
+    if (t.right != null) {
+      if (t.left == null) {
+        sb.append("()");
+      }
+      sb.append('(');
+      tree2str(t.right, sb);
+      sb.append(')');
+    }
+  }
+}
