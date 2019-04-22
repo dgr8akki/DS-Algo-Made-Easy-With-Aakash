42b442515bd9dc05a6b41a6f1e17a42d91368a0e
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-22 11:13:10 +0530

    [LeetCode][#102] Binary Tree level order refactor

--- Leetcode/#102 BinaryTreeLevelOrder.java
+++ Leetcode/#102 BinaryTreeLevelOrder.java
@@ -1,7 +1,3 @@
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
 class Solution {
   public List<List<Integer>> levelOrder(TreeNode root) {
     Queue<TreeNode> mainQueue = new LinkedList<>();
