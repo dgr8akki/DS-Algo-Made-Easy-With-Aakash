07a1f406ccfac6a01746e91f1838985eb0c96ef7
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-22 22:28:33 +0530

    [LeetCode][#90] Add subset - II problem

---
+++ Leetcode/#90 SubsetII.java
@@ -0,0 +1,19 @@
+class Solution {
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+  }
+
+  private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+    list.add(new ArrayList<>(tempList));
+    for (int i = start; i < nums.length; i++) {
+      if (i > start && nums[i] == nums[i - 1])
+        continue;
+      tempList.add(nums[i]);
+      backtrack(list, tempList, nums, i + 1);
+      tempList.remove(tempList.size() - 1);
+    }
+  }
+}
