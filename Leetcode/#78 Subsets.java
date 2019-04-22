f877f12a19850c6cc60b29219dd95bc6748639c1
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-22 22:25:49 +0530

    [LeetCode][#78] Refactor #78

--- Leetcode/#78 Subsets.java
+++ Leetcode/#78 Subsets.java
@@ -1,17 +1,18 @@
 class Solution {
   public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
+    subsets(list, new ArrayList<>(), nums, 0);
     return list;
   }
 
-  private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
-    list.add(new ArrayList<>(tempList));
-    for (int i = start; i < nums.length; i++) {
-      tempList.add(nums[i]);
-      backtrack(list, tempList, nums, i + 1);
-      tempList.remove(tempList.size() - 1);
+  private void subsets(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+    if (start == nums.length) {
+      list.add(new ArrayList<>(tempList));
+      return;
     }
+    subsets(list, tempList, nums, start + 1);
+    tempList.add(nums[start]);
+    subsets(list, tempList, nums, start + 1);
+    tempList.remove(tempList.size() - 1);
   }
 }
