de24a2dbe55a6e1fe083c3652c074a3a864710be
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-23 21:19:10 +0530

    [LeetCode][#981] Time Based Key Value Store

---
+++ Leetcode/#981 TimeBasedKeyValue.java
@@ -0,0 +1,25 @@
+public class TimeMap {
+
+  private Map<String, TreeMap<Integer, String>> map;
+
+  public TimeMap() {
+    map = new HashMap<>();
+  }
+
+  public void set(String key, String value, int timestamp) {
+    if (!map.containsKey(key))
+      map.put(key, new TreeMap<>());
+
+    map.get(key).put(timestamp, value);
+  }
+
+  public String get(String key, int timestamp) {
+    TreeMap<Integer, String> treeMap = map.get(key);
+    if (treeMap == null)
+      return "";
+    Integer floor = treeMap.floorKey(timestamp);
+    if (floor == null)
+      return "";
+    return treeMap.get(floor);
+  }
+}
