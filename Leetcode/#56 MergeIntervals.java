bdb54f526cd73f5b13e6f3c12e31de1f4d4ead69
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-23 21:17:32 +0530

    [LeetCode][#56] Merge Intervals

---
+++ Leetcode/#56 MergeIntervals.java
@@ -0,0 +1,38 @@
+class Solution {
+  public int[][] merge(int[][] intervals) {
+    if (intervals.length == 0)
+      return new int[0][0];
+    java.util.Arrays.sort(intervals, new java.util.Comparator<int[]>() {
+      public int compare(int[] a, int[] b) {
+        return Integer.compare(a[0], b[0]);
+      }
+    });
+
+    List<List<Integer>> temp = new ArrayList<>();
+    int start = intervals[0][0];
+    int end = intervals[0][1];
+    for (int[] interval : intervals) {
+      if (interval[0] <= end)
+        end = Math.max(end, interval[1]);
+      else {
+        List<Integer> x = new ArrayList<Integer>();
+        x.add(start);
+        x.add(end);
+        temp.add(x);
+        start = interval[0];
+        end = interval[1];
+      }
+    }
+    List<Integer> x = new ArrayList<Integer>();
+    x.add(start);
+    x.add(end);
+    temp.add(x);
+
+    int[][] res = new int[temp.size()][2];
+    for (int i = 0; i < temp.size(); i++) {
+      res[i][0] = temp.get(i).get(0);
+      res[i][1] = temp.get(i).get(1);
+    }
+    return res;
+  }
+}
