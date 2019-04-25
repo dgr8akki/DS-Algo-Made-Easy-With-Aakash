ba0b23bb8930dfefa0ec314dc1581bfcba03e23a
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-25 21:16:35 +0530

    [LeetCode][#506] Relative Ranks

---
+++ Leetcode/#506 RelativeRanks.java
@@ -0,0 +1,20 @@
+class Solution {
+  public String[] findRelativeRanks(int[] nums) {
+    String[] medals = { "Gold Medal", "Silver Medal", "Bronze Medal" };
+    String[] result = new String[nums.length];
+    int[] copy = Arrays.copyOf(nums, nums.length);
+    Map<Integer, Integer> map = new HashMap<>();
+    Arrays.sort(copy);
+    for (int i = 0; i < nums.length; i++) {
+      map.put(copy[i], nums.length - 1 - i);
+    }
+
+    int rank = 0;
+    for (int i = 0; i < nums.length; i++) {
+      rank = map.get(nums[i]);
+      result[i] = rank < 3 ? medals[rank] : String.valueOf(rank + 1);
+    }
+
+    return result;
+  }
+}
