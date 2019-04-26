ee2be2935b418471109308a23ef75b96b73c88a3
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-26 21:26:46 +0530

    [LeetCode][#437] Path sum - III

---
+++ Leetcode/#437 PathSumThree.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int pathSum(TreeNode root, int sum) {
+    if (root == null)
+      return 0;
+    return dfs(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+  }
+
+  public int dfs(TreeNode root, int sum) {
+    if (root == null)
+      return 0;
+
+    int temp = root.val == sum ? 1 : 0;
+    int left = dfs(root.left, sum - root.val);
+    int right = dfs(root.right, sum - root.val);
+
+    return temp + left + right;
+  }
+}
