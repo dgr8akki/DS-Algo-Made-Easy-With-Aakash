4bf468b331e3e1a1d5c44ca16f58aa8304bceceb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-27 14:15:53 +0530

    [LeetCode][#201] Bitwise AND of range

---
+++ Leetcode/#201 BiwiseANDofNumberRange.java
@@ -0,0 +1,11 @@
+public class Solution {
+  public int rangeBitwiseAnd(int m, int n) {
+    int diffBits = 0;
+    while (m != n) {
+      m >>= 1;
+      n >>= 1;
+      diffBits++;
+    }
+    return n << diffBits;
+  }
+}
