f64af1c842454730a2441105deaae414de76e6b6
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-27 14:17:06 +0530

    [LeetCode][#477] Total Hamming Distance

---
+++ Leetcode/#477 TotalHammingDistance.java
@@ -0,0 +1,10 @@
+class Solution {
+  public int totalHammingDistance(int[] nums) {
+    int res = 0;
+    for (int i = 0; i < nums.length; i++)
+      for (int j = i + 1; j < nums.length; j++)
+        if (i != j)
+          res += Integer.bitCount(nums[i] ^ nums[j]);
+    return res;
+  }
+}
