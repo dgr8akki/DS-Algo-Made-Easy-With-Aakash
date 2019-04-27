d5acbe53f9a135c71fdd07cc20cb114ff77c5b7d
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-27 14:15:23 +0530

    [LeetCode][#187] Repeated DNA

---
+++ Leetcode/#187 RepeatedDNA.java
@@ -0,0 +1,11 @@
+class Solution {
+  public List<String> findRepeatedDnaSequences(String s) {
+    Set seen = new HashSet(), repeated = new HashSet();
+    for (int i = 0; i + 9 < s.length(); i++) {
+      String ten = s.substring(i, i + 10);
+      if (!seen.add(ten))
+        repeated.add(ten);
+    }
+    return new ArrayList(repeated);
+  }
+}
