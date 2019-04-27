0080ec11b9f633b466900027e16010f7d3941a10
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-27 14:18:02 +0530

    [LeetCode][#762] Prime number of set bit

---
+++ Leetcode/#762 PrimeNumberOfSetBit.java
@@ -0,0 +1,10 @@
+class Solution {
+  public int countPrimeSetBits(int L, int R) {
+    int count = 0;
+    Set<Integer> primes = new HashSet<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29));
+    for (int num = L; num <= R; num++)
+      if (primes.contains(Integer.bitCount(num)))
+        count++;
+    return count;
+  }
+}
