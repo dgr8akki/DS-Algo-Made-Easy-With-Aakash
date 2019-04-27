a4ac2d7ded69de1ddc7fe628e4e2e2e582393a0f
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-27 14:16:24 +0530

    [LeetCode][#219] Contains Duplicate

---
+++ Leetcode/#219 ContainsDuplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] != nums[j])
+          continue;
+        if (Math.abs(i - j) <= k)
+          return true;
+      }
+    }
+    return false;
+  }
+}
