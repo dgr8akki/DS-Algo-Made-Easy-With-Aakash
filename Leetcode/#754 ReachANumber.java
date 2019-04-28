61dd78b021bbd717b908d7b416778fdbf061a606
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-28 09:22:10 +0530

    [LeetCode][#754] Reach a number

---
+++ Leetcode/#754 ReachANumber.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int reachNumber(int target) {
+    target = Math.abs(target);
+    int step = 0;
+    int sum = 0;
+    while (sum < target) {
+      step++;
+      sum += step;
+    }
+    while ((sum - target) % 2 != 0) {
+      step++;
+      sum += step;
+    }
+    return step;
+  }
+}
