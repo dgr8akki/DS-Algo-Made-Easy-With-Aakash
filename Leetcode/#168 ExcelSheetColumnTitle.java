b3c890cee3a59ece56a5894af2ad10f2d9351c82
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-28 09:17:28 +0530

    [LeetCode][#168] Excel Sheet Column Title

---
+++ Leetcode/#168 ExcelSheetColumnTitle.java
@@ -0,0 +1,20 @@
+class Solution {
+  public String convertToTitle(int n) {
+    StringBuilder builder = new StringBuilder();
+    while (n > 0) {
+      n--;
+      builder.insert(0, (char) ('A' + n % 26));
+      n /= 26;
+    }
+    return builder.toString();
+  }
+
+  public int titleToNumber(String s) {
+    int result = 0;
+    for (int i = 0; i < s.length(); i++) {
+      result *= 26;
+      result += (s.charAt(i) - 'A' + 1);
+    }
+    return result;
+  }
+}
