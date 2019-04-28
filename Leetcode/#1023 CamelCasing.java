3e8c64c449cbba7e9f6c4785f4944a7b897ccc6b
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-28 09:16:54 +0530

    [LeetCode][#1023] CamelCasing

---
+++ Leetcode/#1023 CamelCasing.java
@@ -0,0 +1,26 @@
+class Solution {
+  public List<Boolean> camelMatch(String[] queries, String pattern) {
+    List<Boolean> res = new ArrayList<>();
+
+    char[] patternArr = pattern.toCharArray();
+    for (String query : queries) {
+      boolean isMatch = match(query.toCharArray(), patternArr);
+      res.add(isMatch);
+    }
+
+    return res;
+  }
+
+  private boolean match(char[] queryArr, char[] patternArr) {
+    int j = 0;
+    for (int i = 0; i < queryArr.length; i++) {
+      if (j < patternArr.length && queryArr[i] == patternArr[j]) {
+        j++;
+      } else if (queryArr[i] >= 'A' && queryArr[i] <= 'Z') {
+        return false;
+      }
+    }
+
+    return j == patternArr.length;
+  }
+}
