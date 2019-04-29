c4b9ffa8d6a1beb232d88cd04503850b9f182deb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-29 07:21:45 +0530

    [LeetCode][#396] Rotate function

---
+++ Leetcode/#396 RotateFunction.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int maxRotateFunction(int[] A) {
+    if (A.length == 0) {
+      return 0;
+    }
+
+    int sum = 0, iteration = 0, len = A.length;
+
+    for (int i = 0; i < len; i++) {
+      sum += A[i];
+      iteration += (A[i] * i);
+    }
+
+    int max = iteration;
+    for (int j = 1; j < len; j++) {
+      iteration = iteration - sum + A[j - 1] * len;
+      max = Math.max(max, iteration);
+    }
+
+    return max;
+  }
+}
