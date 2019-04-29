1907b2e3616bb5d85e060b2de0b2299eeac620f7
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-29 07:18:20 +0530

    [LeetCode][#67] Add Binary

--- Leetcode/#67 AddBinary.java
+++ Leetcode/#67 AddBinary.java
@@ -1,13 +1,18 @@
-class Solution {
-    public static String addBinary(String a, String b) {
-        int an = Integer.parseInt(a, 2);
-        int bn = Integer.parseInt(b, 2);
-        return Integer.toBinaryString(an + bn);
+public class Solution {
+  public String addBinary(String a, String b) {
+    StringBuilder sb = new StringBuilder();
+    int i = a.length() - 1, j = b.length() - 1, carry = 0;
+    while (i >= 0 || j >= 0) {
+      int sum = carry;
+      if (j >= 0)
+        sum += b.charAt(j--) - '0';
+      if (i >= 0)
+        sum += a.charAt(i--) - '0';
+      sb.append(sum % 2);
+      carry = sum / 2;
     }
-
-    public static void main(String[] args) {
-      String a = "11";
-      String b = "1";
-      System.out.println(addBinary(a, b));
-    }
+    if (carry != 0)
+      sb.append(carry);
+    return sb.reverse().toString();
+  }
 }
