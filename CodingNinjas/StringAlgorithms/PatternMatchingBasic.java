f9ddffebbfffcadd86996d992386e82293a26078
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-04-30 21:08:02 +0530

    [CodingNinjas][StringsAlgorithms] Add Pattern matching basic algorithm

---
+++ CodingNinjas/StringAlgorithms/PatternMatchingBasic.java
@@ -0,0 +1,25 @@
+/**
+ * PatternMatchingBasic
+ */
+public class PatternMatchingBasic {
+  public static void main(String[] args) {
+    String text = "The quick brown fox jumps";
+    String pattern = "T";
+    System.out.println(patternMatching(text, pattern));
+  }
+
+  private static int patternMatching(String text, String pattern) {
+    int textLen = text.length();
+    int patLen = pattern.length();
+    for (int i = 0; i < (textLen - patLen); i++) {
+      int j = 0;
+      for (; j < patLen; j++) {
+        if (text.charAt(i + j) != pattern.charAt(j))
+          break;
+      }
+      if (j == patLen)
+        return i;
+    }
+    return -1;
+  }
+}
