3a5682c4e9fdc81614646bd9268a280d3c6be56c
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-01 21:53:26 +0530

    [LeetCode][#783] Minimum distance in BST

---
+++ Leetcode/#783 MinimumDistanceBST.java
@@ -0,0 +1,20 @@
+class Solution {
+  int min = Integer.MAX_VALUE;
+  int prev = -1;
+
+  public int minDiffInBST(TreeNode root) {
+    if (root == null)
+      return min;
+
+    minDiffInBST(root.left);
+
+    if (prev != -1) {
+      min = Math.min(min, root.val - prev);
+    }
+    prev = root.val;
+
+    minDiffInBST(root.right);
+
+    return min;
+  }
+}
