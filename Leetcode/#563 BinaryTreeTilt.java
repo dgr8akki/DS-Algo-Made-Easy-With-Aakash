632946a97c64e71fd7167bed93c778c5f2ee9918
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-01 21:47:34 +0530

    [LeetCode][#563] Binary Tree Tilt

--- Leetcode/#563 BinaryTreeTilt.java
+++ Leetcode/#563 BinaryTreeTilt.java
@@ -1,22 +1,21 @@
 class Solution {
+  int res = 0;
+
   public int findTilt(TreeNode root) {
+    if (root == null)
+      return 0;
+    findTiltUtil(root);
+    return res;
+  }
+
+  public int findTiltUtil(TreeNode root) {
     int selfTilt = 0, leftTilt = 0, rightTilt = 0;
     if (root == null)
       return 0;
-    if (root.left == null && root.right == null)
-      return 0;
-    if (root.left == null && root.right != null) {
-      rightTilt = findTilt(root.right);
-      selfTilt = Math.abs(root.right.val);
-    } else if (root.left != null && root.right == null) {
-      leftTilt = findTilt(root.left);
-      selfTilt = Math.abs(root.left.val);
-    } else if (root.left != null && root.right != null) {
-      rightTilt = findTilt(root.right);
-      leftTilt = findTilt(root.left);
-      selfTilt = Math.abs(root.left.val - root.right.val);
-    }
-
+    selfTilt = root.val;
+    leftTilt = findTiltUtil(root.left);
+    rightTilt = findTiltUtil(root.right);
+    res += Math.abs(leftTilt - rightTilt);
     return selfTilt + leftTilt + rightTilt;
   }
 }
