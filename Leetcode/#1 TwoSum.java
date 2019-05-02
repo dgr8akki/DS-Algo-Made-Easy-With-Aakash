33193f20ee4718c5674ca439faaa5841f1233769
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-02 21:42:40 +0530

    [LeetCode][#1] Refactor

--- Leetcode/#1 TwoSum.java
+++ Leetcode/#1 TwoSum.java
@@ -1,12 +1,14 @@
 class Solution {
-  public int[] twoSum(int[] nums, int target) {
+  public int[] twoSum(int[] numbers, int target) {
     int[] result = new int[2];
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++)
-        if (nums[i] + nums[j] == target) {
-          result[0] = i;
-          result[1] = j;
-        }
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numbers.length; i++) {
+      if (map.containsKey(target - numbers[i])) {
+        result[1] = i;
+        result[0] = map.get(target - numbers[i]) - 1;
+        return result;
+      }
+      map.put(numbers[i], i + 1);
     }
     return result;
   }
