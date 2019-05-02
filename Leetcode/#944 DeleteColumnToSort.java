4d3c0f2984e8b93b906ba60a78960740257fe055
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-02 21:45:06 +0530

    [LeetCode][#944] Delete column to sort

---
+++ Leetcode/#944 DeleteColumnToSort.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int minDeletionSize(String[] A) {
+    if (A.length <= 1 || A[0].length() <= 1)
+      return 0;
+    int res = 0;
+    for (int i = 0; i < A[0].length(); i++) {
+      for (int j = 1; j < A.length; j++) {
+        if (A[j].charAt(i) < A[j - 1].charAt(i)) {
+          res++;
+          break;
+        }
+      }
+    }
+    return res;
+  }
+}
