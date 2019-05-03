d78d844c37944fd6bfe60135c47283f7530b76a7
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-03 22:49:44 +0530

    [LeetCode][#690] Employee Importance

---
+++ Leetcode/#690 EmployeeImportance.java
@@ -0,0 +1,22 @@
+/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+  int total = 0;
+
+  public int getImportance(List<Employee> employees, int id) {
+    Employee manager = employees.stream().filter(e -> e.id == id).collect(Collectors.toList()).get(0);
+    total += manager.importance;
+    manager.subordinates.forEach(subId -> getImportance(employees, subId));
+    return total;
+  }
+}
