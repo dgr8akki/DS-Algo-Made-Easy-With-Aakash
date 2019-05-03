4dbda5040a01c1742e1766ea307b7b8628086439
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-03 22:49:23 +0530

    [LeetCode][#598] Range Addition

---
+++ Leetcode/#598 RangeAddition.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int maxCount(int m, int n, int[][] ops) {
+    for (int i = 0; i < ops.length; i++) {
+      if (ops[i][0] < m)
+        m = ops[i][0];
+      if (ops[i][1] < n)
+        n = ops[i][1];
+    }
+    return m * n;
+  }
+}
