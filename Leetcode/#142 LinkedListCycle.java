1e81f0f690a2b1d9b3a2e85abb256de4a497b494
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-04 18:25:59 +0530

    [LeetCode][#142] Linked List Cycle

---
+++ Leetcode/#142 LinkedListCycle.java
@@ -0,0 +1,21 @@
+public class Solution {
+  public ListNode detectCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (fast == slow) {
+        ListNode slow2 = head;
+        while (slow2 != slow) {
+          slow = slow.next;
+          slow2 = slow2.next;
+        }
+        return slow;
+      }
+    }
+    return null;
+  }
+}
