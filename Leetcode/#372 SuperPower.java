e8e61e22872ccd381854035126ad141afd6fa55f
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-04 17:25:56 +0530

    [LeetCode][#372] Super power

---
+++ Leetcode/#372 SuperPower.java
@@ -0,0 +1,19 @@
+class Solution {
+  int ans = 1;
+
+  public int superPow(int a, int[] b) {
+    int res = 1;
+    for (int i = 0; i < b.length; i++) {
+      res = pow(res, 10) * pow(a, b[i]) % 1337;
+    }
+    return res;
+  }
+
+  public int pow(int a, int b) {
+    if (b == 0)
+      return 1;
+    if (b == 1)
+      return a % 1337;
+    return pow(a % 1337, b / 2) * pow(a % 1337, b - b / 2) % 1337;
+  }
+}
