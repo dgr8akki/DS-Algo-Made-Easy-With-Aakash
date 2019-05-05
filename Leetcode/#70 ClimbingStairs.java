c65e42dbcd3fae4e455ad6cf2f26cb55f3e963af
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-05 14:50:12 +0530

    [LeetCode][#70] Climbing Stairs

---
+++ Leetcode/#70 ClimbingStairs.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int climbStairs(int n) {
+    if (n == 0 || n == 1)
+      return 1;
+    int lastSecond = 1, last = 1, current = 0;
+    for (int i = 2; i <= n; i++) {
+      current = lastSecond + last;
+      lastSecond = last;
+      last = current;
+    }
+    return current;
+  }
+}
