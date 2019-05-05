ddf03f4a449483e6c5dd8ee6b498eec2f82c71e1
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-05 14:43:32 +0530

    [LeetCode][#232] Queue using Stacks

--- Leetcode/#232 QueueUsingStack.java
+++ Leetcode/#232 QueueUsingStack.java
@@ -1,47 +1,39 @@
 class MyQueue {
 
-    /** Initialize your data structure here. */
-    Stack<Integer> insertionStack, deletionStack;
-    public MyQueue() {
-        insertionStack = new Stack<>();
-        deletionStack = new Stack<>();
-    }
-    
-    /** Push element x to the back of queue. */
-    public void push(int x) {
-        insertionStack.push(x);
-    }
-    
-    /** Removes the element from in front of queue and returns that element. */
-    public int pop() {
-        if(!deletionStack.isEmpty())
-          return deletionStack.pop();
-        while(!deletionStack.isEmpty())
-          deletionStack.push(insertionStack.pop());
-      return deletionStack.pop();
-    }
-    
-    /** Get the front element. */
-    public int peek() {
-       if(!deletionStack.isEmpty())
-          return deletionStack.peek();
-        while(!deletionStack.isEmpty())
-          deletionStack.push(insertionStack.pop());
-      return deletionStack.peek();
-        
-    }
-    
-    /** Returns whether the queue is empty. */
-    public boolean empty() {
-        return insertionStack.isEmpty() && deletionStack.isEmpty();
-    }
-}
+  /** Initialize your data structure here. */
+  Stack<Integer> insertionStack, deletionStack;
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
+  public MyQueue() {
+    insertionStack = new Stack<>();
+    deletionStack = new Stack<>();
+  }
+
+  /** Push element x to the back of queue. */
+  public void push(int x) {
+    insertionStack.push(x);
+  }
+
+  /** Removes the element from in front of queue and returns that element. */
+  public int pop() {
+    if (!deletionStack.isEmpty())
+      return deletionStack.pop();
+    while (!insertionStack.isEmpty())
+      deletionStack.push(insertionStack.pop());
+    return deletionStack.pop();
+  }
+
+  /** Get the front element. */
+  public int peek() {
+    if (!deletionStack.isEmpty())
+      return deletionStack.peek();
+    while (!insertionStack.isEmpty())
+      deletionStack.push(insertionStack.pop());
+    return deletionStack.peek();
+
+  }
+
+  /** Returns whether the queue is empty. */
+  public boolean empty() {
+    return insertionStack.isEmpty() && deletionStack.isEmpty();
+  }
+}
