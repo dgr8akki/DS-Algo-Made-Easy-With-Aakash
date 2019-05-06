17316e4e9c3cfee05203f54e7622229a2e95e026
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-06 22:50:23 +0530

    [LeetCode][#784] Letter Permutation

---
+++ Leetcode/#784 LetterPermutation.java
@@ -0,0 +1,24 @@
+class Solution {
+  public List<String> letterCasePermutation(String S) {
+    if (S == null || S.length() == 0)
+      return new ArrayList<>();
+    List<String> ret = new LinkedList<>();
+    helper(S.toCharArray(), 0, ret);
+    return ret;
+  }
+
+  public void helper(char[] chars, int pos, List<String> ret) {
+    if (pos == chars.length) {
+      ret.add(new String(chars));
+      return;
+    }
+    if (Character.isDigit(chars[pos])) {
+      helper(chars, pos + 1, ret);
+      return;
+    }
+    chars[pos] = Character.toLowerCase(chars[pos]);
+    helper(chars, pos + 1, ret);
+    chars[pos] = Character.toUpperCase(chars[pos]);
+    helper(chars, pos + 1, ret);
+  }
+}
