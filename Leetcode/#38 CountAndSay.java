d2da5bac5303df1e65250c9cfda27a75b098708e
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-06 22:48:10 +0530

    [LeetCode][#38] Count and Say

---
+++ Leetcode/#38 CountAndSay.java
@@ -0,0 +1,22 @@
+class Solution {
+  public String countAndSay(int n) {
+    StringBuilder s = new StringBuilder("1");
+    for (int i = 0; i < n - 1; ++i) {
+      StringBuilder sb = new StringBuilder();
+      int count = 1;
+      char c = s.charAt(0);
+      s.append("a");
+      for (int j = 1; j < s.length(); ++j) {
+        if (s.charAt(j) == c) {
+          count++;
+        } else {
+          sb.append("" + count + c);
+          count = 1;
+          c = s.charAt(j);
+        }
+      }
+      s = sb;
+    }
+    return s.toString();
+  }
+}
