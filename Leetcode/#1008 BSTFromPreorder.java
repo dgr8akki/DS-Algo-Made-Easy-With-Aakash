fba10a8f380415ec7d06e0c285d005b9ae6d7826
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-07 22:12:54 +0530

    [LeetCode][#1008] Binary Tree From Pre-Order

---
+++ Leetcode/#1008 BSTFromPreorder.java
@@ -0,0 +1,21 @@
+class Solution {
+  public TreeNode bstFromPreorder(int[] preorder) {
+    return bstFromPreorderUtil(preorder, 0, preorder.length - 1);
+  }
+
+  private TreeNode bstFromPreorderUtil(int[] A, int start, int end) {
+    if (start > end)
+      return null;
+    TreeNode root = new TreeNode(A[start]);
+    if (start == end)
+      return root;
+    int index;
+    for (index = start + 1; index <= end; index++)
+      if (A[index] > root.val)
+        break;
+
+    root.left = bstFromPreorderUtil(A, start + 1, index - 1);
+    root.left = bstFromPreorderUtil(A, index, end);
+    return root;
+  }
+}
