7b2028cfa7ff6dfbe99dd5192f18558894031efc
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-08 22:05:58 +0530

    [LeetCode][#1038] BST to GST

---
+++ Leetcode/#1038 BstToGst.java
@@ -0,0 +1,13 @@
+class Solution {
+  int sum = 0;
+
+  public TreeNode bstToGst(TreeNode root) {
+    if (root == null)
+      return null;
+    bstToGst(root.right);
+    sum += root.val;
+    root.val = sum;
+    bstToGst(root.left);
+    return root;
+  }
+}
