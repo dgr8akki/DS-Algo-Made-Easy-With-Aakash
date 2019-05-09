00366e53efee2ff068135df35c8cd45fa2a7eb63
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-09 21:24:42 +0530

    [#173] BST Iterator

---
+++ Leetcode/#173 BSTIterator.java
@@ -0,0 +1,22 @@
+public class BSTIterator {
+  private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+  public BSTIterator(TreeNode root) {
+    pushAll(root);
+  }
+
+  public boolean hasNext() {
+    return !stack.isEmpty();
+  }
+
+  public int next() {
+    TreeNode tmpNode = stack.pop();
+    pushAll(tmpNode.right);
+    return tmpNode.val;
+  }
+
+  private void pushAll(TreeNode node) {
+    for (; node != null; stack.push(node), node = node.left)
+      ;
+  }
+}
