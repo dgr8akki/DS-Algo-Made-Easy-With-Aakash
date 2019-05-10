553dd6d39d2cbfb589ac1c742fce90810fecf5eb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-10 23:35:10 +0530

    [LeetCode][#114] Flatten a tree

---
+++ Leetcode/#114 FlattenTree.java
@@ -0,0 +1,26 @@
+class Solution {
+
+  private TreeNode prev;
+
+  public void flatten(TreeNode root) {
+    if (root == null)
+      return;
+    prev = null;
+    preorder(root);
+  }
+
+  private void preorder(TreeNode curr) {
+    if (curr == null)
+      return;
+
+    if (prev != null) {
+      prev.right = curr;
+      prev.left = null;
+    }
+
+    prev = curr;
+    TreeNode right = curr.right;
+    preorder(curr.left);
+    preorder(right);
+  }
+}
