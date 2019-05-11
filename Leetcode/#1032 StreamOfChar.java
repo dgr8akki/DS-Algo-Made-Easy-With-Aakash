a538e1d0d0f0a484950265b7fd2588f98a0f0756
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-11 22:39:55 +0530

    [LeetCode][#1032] Stream of char

---
+++ Leetcode/#1032 StreamOfChar.java
@@ -0,0 +1,75 @@
+class StreamChecker {
+
+  class Trie {
+    char val;
+    String word;
+    Trie[] children;
+
+    Trie(char val) {
+      this.val = val;
+      this.children = new Trie[26];
+    }
+  }
+
+  private Trie root;
+  private Set<Character> charSet;
+  private int maxLen = Integer.MIN_VALUE;
+  private String list = "";
+
+  public StreamChecker(String[] words) {
+    this.root = new Trie('*');
+    this.charSet = new HashSet<>();
+    for (String w : words) {
+      Trie runner = this.root;
+      if (build(w, runner)) {
+        this.maxLen = Math.max(this.maxLen, w.length());
+      }
+    }
+  }
+
+  private boolean build(String w, Trie runner) {
+    if (w == null || w.length() == 0)
+      return false;
+    for (int i = w.length() - 1; i >= 0; i--) {
+      char cur = w.charAt(i);
+      this.charSet.add(cur);
+      if (runner.children[cur - 'a'] == null)
+        runner.children[cur - 'a'] = new Trie(cur);
+      runner = runner.children[cur - 'a'];
+      if (i >= 0 && runner.word == w)
+        return false;
+      if (i == 0)
+        runner.word = w;
+    }
+    return true;
+  }
+
+  public boolean query(char letter) {
+
+    if (!this.charSet.contains(letter)) {
+      this.list = "";
+      return false;
+    }
+
+    this.list = this.list + letter;
+    if (this.list.length() > this.maxLen)
+      this.list = this.list.substring(this.list.length() - maxLen);
+
+    Trie runner = this.root;
+    return contain(runner);
+
+  }
+
+  private boolean contain(Trie runner) {
+
+    for (int i = this.list.length() - 1; i >= 0; i--) {
+      char cur = this.list.charAt(i);
+      if (runner.children[cur - 'a'] == null)
+        return false;
+      runner = runner.children[cur - 'a'];
+      if (runner.word != null)
+        return true;
+    }
+    return false;
+  }
+}
