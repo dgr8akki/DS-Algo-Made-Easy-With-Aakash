4d44058ee56bbe504e7bfc3cd39c3e31b21f9fd3
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-12 13:06:20 +0530

    [LeetCode][#946] Valid Stack sequence

---
+++ Leetcode/#946 ValidStackSequence.java
@@ -0,0 +1,14 @@
+class Solution {
+  public boolean validateStackSequences(int[] pushed, int[] popped) {
+    Stack<Integer> stk = new Stack<>();
+    int i = 0;
+    for (int p : pushed) {
+      stk.push(p);
+      while (!stk.isEmpty() && stk.peek() == popped[i]) {
+        stk.pop();
+        ++i;
+      }
+    }
+    return stk.empty();
+  }
+}
