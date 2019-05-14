08e155447709d0d06c69c2122e9efaa8b7e5fcf4
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-14 09:54:17 +0530

    [LeetCode][#636] Exclusion Time

---
+++ Leetcode/#636 ExlusionTime.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int[] exclusiveTime(int n, List<String> logs) {
+    int[] res = new int[n];
+    Stack<Integer> stack = new Stack<>();
+    int prev = 0;
+    for (String log : logs) {
+      String[] strs = log.split(":");
+      int id = Integer.parseInt(strs[0]);
+      int curr = Integer.parseInt(strs[2]);
+      if (strs[1].equals("start")) {
+        if (!stack.isEmpty()) {
+          res[stack.peek()] += curr - prev;
+        }
+        stack.push(id);
+        prev = curr;
+      } else {
+        res[stack.pop()] += curr - prev + 1;
+        prev = curr + 1;
+      }
+    }
+    return res;
+  }
+}
