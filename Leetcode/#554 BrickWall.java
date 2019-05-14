036a9ceead37bf1a04f25369b91f0272cb56717a
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-14 09:53:40 +0530

    [LeetCode][#554] Brick wall

---
+++ Leetcode/#554 BrickWall.java
@@ -0,0 +1,17 @@
+public class Solution {
+  public int leastBricks(List<List<Integer>> wall) {
+    if (wall.size() == 0)
+      return 0;
+    int count = 0;
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (List<Integer> list : wall) {
+      int length = 0;
+      for (int i = 0; i < list.size() - 1; i++) {
+        length += list.get(i);
+        map.put(length, map.getOrDefault(length, 0) + 1);
+        count = Math.max(count, map.get(length));
+      }
+    }
+    return wall.size() - count;
+  }
+}
