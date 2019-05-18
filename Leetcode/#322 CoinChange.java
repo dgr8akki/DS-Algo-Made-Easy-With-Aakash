6b89979cfac1defa244f69f6dbbc4d6b344babe8
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-18 18:46:48 +0530

    [LeetCode][#322] Coin change

---
+++ Leetcode/#322 CoinChange.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int coinChange(int[] coins, int amount) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i) {
+      dp[i] = Integer.MAX_VALUE;
+      for (int j = 0; j < coins.length; ++j) {
+        if (i - coins[j] >= 0 && dp[i - coins[j]] != -1)
+          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+      if (dp[i] == Integer.MAX_VALUE)
+        dp[i] = -1;
+    }
+    return dp[amount];
+  }
+}
