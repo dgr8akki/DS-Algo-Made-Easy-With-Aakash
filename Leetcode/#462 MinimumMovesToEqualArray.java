dee20f60e7031e0bdff9d8f28f9cdb731ac7a2e8
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-20 23:54:40 +0530

    [LeetCode][#462] Mini moves to equal array

---
+++ Leetcode/#462 MinimumMovesToEqualArray.java
@@ -0,0 +1,13 @@
+public class Solution {
+  public int minMoves2(int[] nums) {
+    Arrays.sort(nums);
+    int i = 0, j = nums.length - 1;
+    int count = 0;
+    while (i < j) {
+      count += nums[j] - nums[i];
+      i++;
+      j--;
+    }
+    return count;
+  }
+}
