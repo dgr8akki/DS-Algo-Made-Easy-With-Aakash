692dd9f4512cc154b115765b170359ce7caf074c
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-21 20:41:46 +0530

    [LeetCode][#1047] Remove adjacent duplicates

---
+++ Leetcode/#1047 RemoveAdjacentDuplicates.java
@@ -0,0 +1,13 @@
+class Solution {
+  public String removeDuplicates(String S) {
+    StringBuilder builder = new StringBuilder();
+    for (char c : S.toCharArray()) {
+      int size = builder.length();
+      if (size > 0 && builder.charAt(size - 1) == c)
+        builder.deleteCharAt(size - 1);
+      else
+        builder.append(c);
+    }
+    return builder.toString();
+  }
+}
