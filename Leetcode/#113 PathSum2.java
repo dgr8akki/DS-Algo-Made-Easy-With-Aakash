4754aff37cb8ccd526851154abd6c6946dbc4782
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-22 22:57:09 +0530

    [LeetCode][#113] Path sum - II

---
+++ Leetcode/#113 PathSum2.java
@@ -0,0 +1,28 @@
+public class Solution {
+  public List<List<Integer>> pathSum(TreeNode root, int sum) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    dfs(root, sum, res, path);
+    return res;
+  }
+
+  public void dfs(TreeNode root, int sum, List<List<Integer>> res, List<Integer> path) {
+    if (root == null)
+      return;
+    path.add(root.val);
+
+    if (root.left == null && root.right == null) {
+      if (root.val == sum)
+        res.add(new ArrayList<Integer>(path));
+      return;
+    }
+    if (root.left != null) {
+      dfs(root.left, sum - root.val, res, path);
+      path.remove(path.size() - 1);
+    }
+    if (root.right != null) {
+      dfs(root.right, sum - root.val, res, path);
+      path.remove(path.size() - 1);
+    }
+  }
+}
