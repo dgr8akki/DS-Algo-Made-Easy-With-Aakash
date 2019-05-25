b714c73a5b0cd3640fda407012421a3111ae7dbb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-05-25 11:58:06 +0530

    [LeetCode][#7] Refactor #7

--- Leetcode/#7 ReverseInteger.java
+++ Leetcode/#7 ReverseInteger.java
@@ -4,7 +4,23 @@ class Solution {
       return 0;
     if (x < 0)
       return -1 * reverse(-1 * x);
-    long v = Long.parseLong(new StringBuilder(Integer.toString(x)).reverse().toString());
-    return v > Integer.MAX_VALUE ? 0 : (int) v;
+    int temp = x;
+    int digits = 0;
+    while (temp > 0) {
+      temp /= 10;
+      digits++;
+    }
+
+    if (Long.parseLong(new StringBuilder(Integer.toString(x)).reverse().toString()) > Integer.MAX_VALUE)
+      return 0;
+
+    int number = 0;
+    for (int i = 0; x > 0; i++) {
+      number += ((x % 10) * (int) Math.pow(10, --digits));
+      x = x / 10;
+      System.out.println(number + " , " + x);
+    }
+
+    return number;
   }
 }
