f1216f1088ff074cece0fcce3fe38aabab138121
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-01 15:10:39 +0530

    [LeetCode][#141] Refactor #141

--- Leetcode/#141 LinkedListCycle.java
+++ Leetcode/#141 LinkedListCycle.java
@@ -1,7 +1,3 @@
-/**
- * Definition for singly-linked list. class ListNode { int val; ListNode next;
- * ListNode(int x) { val = x; next = null; } }
- */
 public class Solution {
   public boolean hasCycle(ListNode head) {
     if (head == null)
