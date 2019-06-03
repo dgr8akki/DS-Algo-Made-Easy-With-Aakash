4d558620bacb482c98f2c8e2a9473b56d5979d1c
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-03 23:33:20 +0530

    [LeetCode][#203] Refactor #203

--- Leetcode/#203 RemoveFromLinkedList.java
+++ Leetcode/#203 RemoveFromLinkedList.java
@@ -1,7 +1,3 @@
-/**
- * Definition for singly-linked list. public class ListNode { int val; ListNode
- * next; ListNode(int x) { val = x; } }
- */
 class Solution {
   public ListNode removeElements(ListNode head, int val) {
     if (head == null)
