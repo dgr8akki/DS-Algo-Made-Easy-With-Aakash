465bc626e2ee9a84d24c43611197344c6f8bddf1
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-05 21:07:08 +0530

    [LeetCode][#235] Refactor #235

--- Leetcode/#235 LCA.java
+++ Leetcode/#235 LCA.java
@@ -1,7 +1,3 @@
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
 class Solution {
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
     if (root == null)
