a217c5c06e0ed67812c7d87dfeb86bd89db43a83
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-08 23:47:40 +0530

    [LeetCode][#189] Refactor #189

--- Leetcode/#189 RotateArray.java
+++ Leetcode/#189 RotateArray.java
@@ -1,25 +1,21 @@
 class Solution {
   public void rotate(int[] nums, int k) {
 
-    if (nums == null || nums.length < 2) {
+    if (nums == null || nums.length < 2)
       return;
-    }
 
     k = k % nums.length;
     reverse(nums, 0, nums.length - k - 1);
     reverse(nums, nums.length - k, nums.length - 1);
     reverse(nums, 0, nums.length - 1);
-
   }
 
-  private void reverse(int[] nums, int i, int j) {
+  private void reverse(int[] nums, int firstIndex, int secondIndex) {
     int tmp = 0;
-    while (i < j) {
-      tmp = nums[i];
-      nums[i] = nums[j];
-      nums[j] = tmp;
-      i++;
-      j--;
+    while (firstIndex < secondIndex) {
+      tmp = nums[firstIndex];
+      nums[firstIndex++] = nums[secondIndex];
+      nums[secondIndex--] = tmp;
     }
   }
 }
