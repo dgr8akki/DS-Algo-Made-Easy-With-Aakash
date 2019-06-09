746f9332f6e97145f537092a1013f12e11bf6307
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-10 01:07:21 +0530

    [LeetCode][#109] Refactor #109

--- Leetcode/#109 SortedListToBinarySearchTree.java
+++ Leetcode/#109 SortedListToBinarySearchTree.java
@@ -1,15 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
 class Solution {
   public TreeNode sortedListToBST(ListNode head) {
     if (head == null)
