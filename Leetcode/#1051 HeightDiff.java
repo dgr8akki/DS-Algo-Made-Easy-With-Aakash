369ca6f9acb266a4af353c773e53dc5bbb938df0
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-13 22:34:33 +0530

    [LeetCode][#1051] Height difference

---
+++ Leetcode/#1051 HeightDiff.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int heightChecker(int[] heights) {
+    int count = 0;
+    int[] copiedArray = heights.clone();
+    Arrays.sort(copiedArray);
+    for (int i = 0; i < heights.length; i++)
+      if (heights[i] != copiedArray[i])
+        count++;
+    return count;
+  }
+}
