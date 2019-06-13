f63a391f860956af0aa976e2e2480d258a12f1a2
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-13 22:34:49 +0530

    [LeetCode][#988] Refactor

--- Leetcode/#988 SmallestStringFromLeaf.java
+++ Leetcode/#988 SmallestStringFromLeaf.java
@@ -4,16 +4,15 @@ class Solution {
   }
 
   public String dfs(TreeNode node, String suffix) {
-    if (null == node) {
+    if (null == node)
       return suffix;
-    }
+
     suffix = "" + (char) ('a' + node.val) + suffix;
-    if (null == node.left && null == node.right) {
+    if (null == node.left && null == node.right)
       return suffix;
-    }
-    if (null == node.left || null == node.right) {
+
+    if (null == node.left || null == node.right)
       return (null == node.left) ? dfs(node.right, suffix) : dfs(node.left, suffix);
-    }
 
     String left = dfs(node.left, suffix);
     String right = dfs(node.right, suffix);
