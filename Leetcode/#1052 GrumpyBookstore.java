fb7422dba69c71bb1dcf622edd76c35ad5da4b31
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-14 22:35:36 +0530

    [#1052] Grumpy Bookstore

---
+++ Leetcode/#1052 GrumpyBookstore.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+    int satisfy = 0, max = 0;
+    for (int i = 0, win = 0; i < grumpy.length; ++i) {
+      if (grumpy[i] == 0)
+        satisfy += customers[i];
+      else
+        win += customers[i];
+
+      if (i >= X && grumpy[i - X] == 1)
+        win -= customers[i - X];
+
+      max = Math.max(win, max);
+    }
+    return satisfy + max;
+  }
+}
