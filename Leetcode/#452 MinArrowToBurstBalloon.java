a3780564195cbe6e52ccc92721c87a261ae67f5d
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-15 16:52:49 +0530

    [LeetCode][#452] Minimum arrow to burst baloons

---
+++ Leetcode/#452 MinArrowToBurstBalloon.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int findMinArrowShots(int[][] points) {
+    if (points == null || points.length == 0)
+      return 0;
+    int count = 0, minEnd = Integer.MAX_VALUE;
+    Arrays.sort(points, (a, b) -> (a[0] - b[0]));
+    for (int[] point : points)
+      if (point[0] > minEnd) {
+        count++;
+        minEnd = point[1];
+      } else
+        minEnd = Math.min(minEnd, point[1]);
+
+    return count + 1;
+  }
+}
