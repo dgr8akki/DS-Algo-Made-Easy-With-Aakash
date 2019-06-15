74ca8c79732b0a8810d9a543d3317fd85eaabca2
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-15 16:52:26 +0530

    [LeetCode][#386] Lexographically ordered

---
+++ Leetcode/#386 Lexographically.java
@@ -0,0 +1,22 @@
+public class Solution {
+  public List<Integer> lexicalOrder(int n) {
+    List<Integer> res = new ArrayList<>();
+    for (int i = 1; i < 10; i++)
+      dfs(i, n, res);
+
+    return res;
+  }
+
+  public void dfs(int current, int n, List<Integer> res) {
+    if (current > n)
+      return;
+    else {
+      res.add(current);
+      for (int i = 0; i < 10; ++i) {
+        if (10 * current + i > n)
+          return;
+        dfs(10 * current + i, n, res);
+      }
+    }
+  }
+}
