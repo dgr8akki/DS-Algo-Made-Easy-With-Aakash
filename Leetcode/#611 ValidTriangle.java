165f28170e74cc09dbd5b3eb46e4b48ef23ba526
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-16 20:38:48 +0530

    [LeetCode][#611] Valid Triangle

---
+++ Leetcode/#611 ValidTriangle.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int triangleNumber(int[] nums) {
+    Arrays.sort(nums);
+    int cnt = 0;
+    for (int i = nums.length - 1; i >= 2; --i) {
+      int start = 0;
+      int end = i - 1;
+      while (start < end) {
+        if (nums[start] + nums[end] <= nums[i]) {
+          ++start;
+          continue;
+        }
+
+        cnt += (end - start);
+        --end;
+      }
+    }
+
+    return cnt;
+  }
+}
