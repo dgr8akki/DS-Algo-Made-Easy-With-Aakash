c94a948ec08c6bf121ddf2a3459e9f810e590d48
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-18 22:32:39 +0530

    [LeetCode][#179] Largest Number

---
+++ Leetcode/#179 LargestNumber.java
@@ -0,0 +1,23 @@
+class Solution {
+  public String largestNumber(int[] num) {
+    StringBuilder res = new StringBuilder();
+    if (num == null || num.length == 0)
+      return null;
+    String[] nums = new String[num.length];
+    for (int i = 0; i < num.length; i++)
+      nums[i] = Integer.toString(num[i]);
+    Comparator<String> comp = new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return (o1 + o2).compareTo(o2 + o1);
+      }
+    };
+    Arrays.sort(nums, comp);
+    if (nums[nums.length - 1].equals("0"))
+      return "0";
+
+    for (int i = nums.length - 1; i >= 0; i--)
+      res.append(nums[i]);
+    return res.toString();
+  }
+}
