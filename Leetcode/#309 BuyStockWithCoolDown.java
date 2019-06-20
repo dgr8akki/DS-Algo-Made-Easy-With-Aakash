7cdb2201ddcc836770a765a5767ec98ca0866eee
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-20 22:36:09 +0530

    [#309] Buy stock with cooldown

---
+++ Leetcode/#309 BuyStockWithCoolDown.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int maxProfit(int[] prices) {
+    if (prices == null || prices.length <= 1)
+      return 0;
+
+    int buyLast = -prices[0], buyCurrent = buyLast;
+    int sellLast = 0, sellCurrent = 0, sellNext = 0;
+
+    for (int i = 1; i < prices.length; i++) {
+      buyLast = Math.max(buyCurrent, sellNext - prices[i]);
+      sellLast = Math.max(sellCurrent, buyCurrent + prices[i]);
+      buyCurrent = buyLast;
+      sellNext = sellCurrent;
+      sellCurrent = sellLast;
+    }
+    return sellLast;
+  }
+}
