bb564e07b16a6fa3e0d877cdc6a004becbb75709
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-06-22 18:10:51 +0530

    [LeetCode][#622] Design Circular Queue

---
+++ Leetcode/#622 DesigbCircularQueue.java
@@ -0,0 +1,43 @@
+class MyCircularQueue {
+  final int[] a;
+  int front, rear = -1, len = 0;
+
+  public MyCircularQueue(int k) {
+    a = new int[k];
+  }
+
+  public boolean enQueue(int val) {
+    if (!isFull()) {
+      rear = (rear + 1) % a.length;
+      a[rear] = val;
+      len++;
+      return true;
+    } else
+      return false;
+  }
+
+  public boolean deQueue() {
+    if (!isEmpty()) {
+      front = (front + 1) % a.length;
+      len--;
+      return true;
+    } else
+      return false;
+  }
+
+  public int Front() {
+    return isEmpty() ? -1 : a[front];
+  }
+
+  public int Rear() {
+    return isEmpty() ? -1 : a[rear];
+  }
+
+  public boolean isEmpty() {
+    return len == 0;
+  }
+
+  public boolean isFull() {
+    return len == a.length;
+  }
+}
