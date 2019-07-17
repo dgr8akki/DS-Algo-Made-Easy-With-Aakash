d8b6727cc01ad6ce34f3c7115ad3d2013bb2bd64
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-07-17 23:12:28 +0530

    [#881] Boats to save people

---
+++ Leetcode/#881 BoatsToSavePeople.java
@@ -0,0 +1,10 @@
+class Solution {
+  public int numRescueBoats(int[] people, int limit) {
+    Arrays.sort(people);
+    int i, j;
+    for (i = 0, j = people.length - 1; i <= j; --j)
+      if (people[i] + people[j] <= limit)
+        ++i;
+    return people.length - 1 - j;
+  }
+}
