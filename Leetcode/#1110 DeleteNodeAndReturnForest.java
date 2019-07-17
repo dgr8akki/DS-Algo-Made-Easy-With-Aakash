ce1e78507b9a3fe23a108626e836417be7c125ae
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-07-17 23:05:24 +0530

    [#1110] Delete Node and return Forest

---
+++ Leetcode/#1110 DeleteNodeAndReturnForest.java
@@ -0,0 +1,24 @@
+class Solution {
+  Set<Integer> toDeleteSet;
+  List<TreeNode> res = new ArrayList<>();
+
+  public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+    res = new ArrayList<>();
+    toDeleteSet = new HashSet<>();
+    for (int i : to_delete)
+      toDeleteSet.add(i);
+    helper(root, true);
+    return res;
+  }
+
+  private TreeNode helper(TreeNode node, boolean is_root) {
+    if (node == null)
+      return null;
+    boolean deleted = toDeleteSet.contains(node.val);
+    if (is_root && !deleted)
+      res.add(node);
+    node.left = helper(node.left, deleted);
+    node.right = helper(node.right, deleted);
+    return deleted ? null : node;
+  }
+}
