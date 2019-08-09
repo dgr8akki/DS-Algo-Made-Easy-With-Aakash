c939d3f422122121b1718b2f91e24fe3ae28706a
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-08-09 22:48:07 +0530

    [LeetCode][#1090] Largest value label

---
+++ Leetcode/#1090 LargestValueLabel.java
@@ -0,0 +1,29 @@
+class Solution {
+  public int largestValsFromLabels(int[] values, int[] labels, int num_wanted, int use_limit) {
+    List<int[]> pairs = new ArrayList<>();
+    Map<Integer, Integer> limits = new HashMap<>();
+    for (int i = 0; i < labels.length; i++) {
+      if (!limits.containsKey(labels[i]))
+        limits.put(labels[i], 0);
+
+      pairs.add(new int[] { values[i], labels[i] });
+    }
+    PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+      @Override
+      public int compare(int[] o1, int[] o2) {
+        return o2[0] - o1[0];
+      }
+    });
+    int max = 0;
+    pq.addAll(pairs);
+    for (int i = 0; i < num_wanted && !pq.isEmpty();) {
+      int[] pair = pq.remove();
+      if (limits.get(pair[1]) < use_limit) {
+        max += pair[0];
+        limits.put(pair[1], limits.get(pair[1]) + 1);
+        i++;
+      }
+    }
+    return max;
+  }
+}
