9478febafb55e9b37ec279471e763b62c4dd57d0
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-08-12 22:49:48 +0530

    [LeetCode][#154] Find minimum in rotated Array

---
+++ Leetcode/#154 FindMinimumInRotatedArray.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int findMin(int[] nums) {
+    int lo = 0, hi = nums.length - 1;
+    while (lo < hi) {
+      int mi = lo + (hi - lo) / 2;
+      if (nums[mi] > nums[hi])
+        lo = mi + 1;
+      else if (nums[mi] < nums[lo]) {
+        hi = mi;
+        lo++;
+      } else
+        hi--;
+    }
+
+    return nums[lo];
+  }
+}
