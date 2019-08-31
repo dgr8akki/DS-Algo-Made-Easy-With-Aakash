bd30710543185b16b706a3b1c375ab32c4f4b5c3
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-08-31 22:37:19 +0530

    [LeetCode][1176] Diet plan performance

---
+++ Leetcode/#1176 DietPlanPerformance.java
@@ -0,0 +1,16 @@
+class Solution {
+  public static int dietPlanPerformance(int[] calories, int k, int lower, int upper) {
+    int score = 0;
+    for (int i = 0; i < calories.length - k + 1; i++) {
+      int totalCalories = 0;
+      for (int j = 0; j < k && i + j < calories.length; j++)
+        totalCalories += calories[i + j];
+
+      if (totalCalories < lower)
+        score--;
+      if (totalCalories > upper)
+        score++;
+    }
+    return score;
+  }
+}
