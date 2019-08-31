2fc7ccbe86d7afd7e2b0db4699959743d8f04c14
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-08-31 22:37:42 +0530

    [LeetCode][241] Diff way to add paranthesis

---
+++ Leetcode/#241 DiffWaysParanthesis.java
@@ -0,0 +1,44 @@
+public class Solution {
+  public List<Integer> diffWaysToCompute(String input) {
+    return dfs(input, new HashMap<String, List<Integer>>());
+  }
+
+  private List<Integer> dfs(String input, Map<String, List<Integer>> map) {
+    if (map.containsKey(input))
+      return map.get(input);
+
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i < input.length(); i++) {
+      char curChar = input.charAt(i);
+      if (curChar == '+' || curChar == '-' || curChar == '*') {
+        String leftPart = input.substring(0, i);
+        String rightPart = input.substring(i + 1);
+        List<Integer> leftResult = dfs(leftPart, map);
+        List<Integer> rightResult = dfs(rightPart, map);
+        for (Integer left : leftResult) {
+          for (Integer right : rightResult) {
+            int curResult = 0;
+            switch (curChar) {
+            case '+':
+              curResult = left + right;
+              break;
+            case '-':
+              curResult = left - right;
+              break;
+            case '*':
+              curResult = left * right;
+              break;
+            }
+            result.add(curResult);
+          }
+        }
+      }
+    }
+
+    if (result.size() == 0)
+      result.add(Integer.parseInt(input));
+
+    map.put(input, result);
+    return result;
+  }
+}
