bbbe5ae9e8a5e70d28d42522aa5a7b6cdec4130e
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-09-01 22:43:40 +0530

    [LeetCode][714] Stock Problem with Fee

---
+++ Leetcode/#714 StockProblemWithFee.java
@@ -0,0 +1,15 @@
+class Solution {
+  public int maxProfit(int[] prices, int fee) {
+    if (prices == null || prices.length == 0)
+      return 0;
+
+    int len = prices.length;
+    int buy = -prices[0], sell = 0;
+    for (int i = 1; i < len; i++) {
+      int tmpbuy = buy;
+      buy = Math.max(buy, sell - prices[i]);
+      sell = Math.max(sell, tmpbuy + prices[i] - fee);
+    }
+    return Math.max(buy, sell);
+  }
+}
