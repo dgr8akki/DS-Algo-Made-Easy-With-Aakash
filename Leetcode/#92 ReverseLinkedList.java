71813e8f59c9c7d32a1864956fbb1e60377873e8
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-09-14 16:30:00 +0530

    [LeetCode][#92] Restore IP address

---
+++ Leetcode/#92 ReverseLinkedList.java
@@ -0,0 +1,31 @@
+
+public class Solution {
+  public ListNode reverseBetween(ListNode head, int m, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    // first part
+    ListNode cur1 = dummy;
+    ListNode pre1 = null;
+    for (int i = 0; i < m; i++) {
+      pre1 = cur1;
+      cur1 = cur1.next;
+    }
+
+    // reverse
+    ListNode cur2 = cur1;
+    ListNode pre2 = pre1;
+    ListNode q2;
+    for (int i = m; i <= n; i++) {
+      q2 = cur2.next;
+      cur2.next = pre2;
+      pre2 = cur2;
+      cur2 = q2;
+    }
+
+    // connect
+    pre1.next = pre2;
+    cur1.next = cur2;
+
+    return dummy.next;
+  }
+}
