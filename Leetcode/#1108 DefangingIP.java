14708353ce4df8f8a5eecd0899db90e59465bf9c
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-10-05 00:41:19 +0530

    [LeetCode][#1108] Defanging IP

---
+++ Leetcode/#1108 DefangingIP.java
@@ -0,0 +1,11 @@
+class Solution {
+  public String defangIPaddr(String address) {
+    StringBuilder builder = new StringBuilder();
+    for (char c : address.toCharArray())
+      if (c == '.')
+        builder.append("[.]");
+      else
+        builder.append(c);
+    return builder.toString();
+  }
+}
