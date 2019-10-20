4d8ed2042da6db13c0ca426634d8ad948d78a6ef
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-10-20 11:31:52 +0530

    [LeetCode][#1232] Check straight line

---
+++ Leetcode/#1232 CheckStraightLine.java
@@ -0,0 +1,10 @@
+class Solution {
+  public boolean checkStraightLine(int[][] co) {
+    int p = co[0][0], q = co[0][1], u = co[1][0], v = co[1][1];
+    for (int[] c : co)
+      if ((c[0] - p) * (c[1] - v) != (c[0] - u) * (c[1] - q))
+        return false;
+
+    return true;
+  }
+}
