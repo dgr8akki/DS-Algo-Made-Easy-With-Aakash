d05418fce049c96db0311e87ad97ccac273579b2
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-11-02 23:22:16 +0530

    Refactor

--- src/main/java/Leetcode/Solution_911_OnlineElection.java
+++ src/main/java/Leetcode/Solution_911_OnlineElection.java
@@ -1,18 +1,24 @@
 package Leetcode;
 
-class TopVotedCandidate {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-  Map<Integer, Integer> m = new HashMap<>();
-  int[] time;
+class Solution_911_OnlineElection {
 
-  public TopVotedCandidate(int[] persons, int[] times) {
-    int n = persons.length, lead = -1;
+  private final Map<Integer, Integer> m = new HashMap<>();
+  private final int[] time;
+
+  public Solution_911_OnlineElection(int[] persons, int[] times) {
+    int n = persons.length;
+    int lead = -1;
     Map<Integer, Integer> count = new HashMap<>();
     time = times;
     for (int i = 0; i < n; ++i) {
       count.put(persons[i], count.getOrDefault(persons[i], 0) + 1);
-      if (i == 0 || count.get(persons[i]) >= count.get(lead))
+      if (i == 0 || count.get(persons[i]) >= count.get(lead)) {
         lead = persons[i];
+      }
       m.put(times[i], lead);
     }
   }
