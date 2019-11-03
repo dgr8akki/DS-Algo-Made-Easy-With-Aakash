89f412edd22c5e4ac8ab504b70f1f8f65f8d23eb
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-11-03 23:22:25 +0530

    Refactor

--- src/main/java/Leetcode/Solution_916_WordSubset.java
+++ src/main/java/Leetcode/Solution_916_WordSubset.java
@@ -1,30 +1,39 @@
 package Leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution_916_WordSubset {
+  private static int[] counter(String word) {
+    int[] count = new int[26];
+    for (char c : word.toCharArray()) {
+      count[c - 'a']++;
+    }
+    return count;
+  }
+
   public List<String> wordSubsets(String[] A, String[] B) {
-    int[] uni = new int[26], tmp;
+    int[] uni = new int[26];
+    int[] tmp;
     int i;
     for (String b : B) {
-      tmp = counter(b);
-      for (i = 0; i < 26; ++i)
+      tmp = Solution_916_WordSubset.counter(b);
+      for (i = 0; i < 26; ++i) {
         uni[i] = Math.max(uni[i], tmp[i]);
+      }
     }
     List<String> res = new ArrayList<>();
     for (String a : A) {
-      tmp = counter(a);
-      for (i = 0; i < 26; ++i)
-        if (tmp[i] < uni[i])
+      tmp = Solution_916_WordSubset.counter(a);
+      for (i = 0; i < 26; ++i) {
+        if (tmp[i] < uni[i]) {
           break;
-      if (i == 26)
+        }
+      }
+      if (i == 26) {
         res.add(a);
+      }
     }
     return res;
-  }
-
-  int[] counter(String word) {
-    int[] count = new int[26];
-    for (char c : word.toCharArray())
-      count[c - 'a']++;
-    return count;
   }
 }
