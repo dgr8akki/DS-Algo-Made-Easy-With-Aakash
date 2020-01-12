423af4393bb5569a706d0aaaaf36c89d841cf1d3
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-01-12 17:01:31 +0530

    [LeetCode][#1313] Decompress Encoded List

---
+++ Leetcode/Solution_1313_DecompressEncodedList.java
@@ -0,0 +1,9 @@
+class Solution_1313_DecompressEncodedList {
+  public int[] decompressRLElist(int[] nums) {
+    List<Integer> ans = new ArrayList<>();
+    for (int i = 0; i < nums.length; i += 2)
+      for (int j = 0; j < nums[i]; ++j)
+        ans.add(nums[i + 1]);
+    return ans.stream().mapToInt(i -> i).toArray();
+  }
+}
