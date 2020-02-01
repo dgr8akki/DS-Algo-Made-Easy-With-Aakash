ac88e771854e8240651819d3d8ad34331842ded0
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-02-01 18:27:25 +0530

    Solution

---
+++ Leetcode/Solution_1325_DeleteLeavesTree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        return traverse(root, target);
+    }
+    
+    private TreeNode traverse(TreeNode node, int target){
+        if(node == null)
+            return null;
+        node.left = traverse(node.left, target);
+        node.right = traverse(node.right, target);
+        
+        if(node.left == null && node.right == null && node.val == target)
+            return null;
+        else
+            return node;
+    }
+}
