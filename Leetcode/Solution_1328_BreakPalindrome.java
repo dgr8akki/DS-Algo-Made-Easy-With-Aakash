ffe37b5e75a47001b64c378cd3df74f0e539217f
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-02-02 18:27:40 +0530

    Solution

---
+++ Leetcode/Solution_1328_BreakPalindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+public String breakPalindrome(String palindrome) {
+int count=0;
+if(palindrome.length()<2)
+return "";
+StringBuilder check=new StringBuilder(palindrome);
+int mid=check.length()/2;
+for(int i=0;i<mid;i++){
+if(check.charAt(i)>'a'){
+check.setCharAt(i,'a');
+break;
+}
+else if(check.charAt(i)=='a')
+count++;
+if(count==mid)
+check.setCharAt(check.length()-1,'b');
+}
+return check.toString();
+}
+
+}
