158596ce7c7ba7624a16dc21a43e2701da240dbf
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-03-16 23:20:10 +0530

    Leetcode

--- Leetcode/Solution_883_ProjectionOfShapes.java
+++ Leetcode/Solution_883_ProjectionOfShapes.java
@@ -4,11 +4,10 @@ class Solution_883_ProjectionOfShapes {
   public static int projectionArea(int[][] grid) {
     int res = 0;
     int n = grid.length;
-    for (int i = 0, v = 0; i < n; ++i, res += v, v = 0) {
-      for (int j = 0; j < n; ++j) {
+    for (int i = 0, v = 0; i < n; ++i, res += v, v = 0)
+      for (int j = 0; j < n; ++j)
         v = Math.max(v, grid[i][j]);
-      }
-    }
+
     for (int j = 0, v = 0; j < n; ++j, res += v, v = 0) {
       for (int i = 0; i < n; ++i) {
         v = Math.max(v, grid[i][j]);
