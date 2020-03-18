24a8c4abc903d7845156ea8370e1fb6097a7a784
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-03-18 23:21:29 +0530

    Leetcode

--- Leetcode/Solution_951_FlipBinaryTree.java
+++ Leetcode/Solution_951_FlipBinaryTree.java
@@ -5,9 +5,9 @@ class Solution_951_FlipBinaryTree {
     if (root1 == null && root2 == null)
       return true;
 
-    if (root1 != null && root2 == null || root1 == null && root2 != null || root1.val != root2.val) {
+    if (root1 != null && root2 == null || root1 == null && root2 != null || root1.val != root2.val)
       return false;
-    }
+
     return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
         || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
   }
