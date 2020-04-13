18306853c92920076bdb4594d1f370d534574ab2
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-04-13 21:45:20 +0530

    [LeetCode] Refactoring

--- Leetcode/Solution_933_NumberOfRecentCalls.java
+++ Leetcode/Solution_933_NumberOfRecentCalls.java
@@ -5,17 +5,11 @@ class RecentCounter {
   TreeSet<Integer> ts;
 
   public RecentCounter() {
-      ts = new TreeSet<>();
+    ts = new TreeSet<>();
   }
-  
+
   public int ping(int t) {
-      ts.add(t);
-      return ts.tailSet(t - 3000).size();
+    ts.add(t);
+    return ts.tailSet(t - 3000).size();
   }
 }
-
-/**
-* Your RecentCounter object will be instantiated and called as such:
-* RecentCounter obj = new RecentCounter();
-* int param_1 = obj.ping(t);
-*/
