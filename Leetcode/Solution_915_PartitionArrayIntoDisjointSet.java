2e17343825aa1594dad92742d1e4d856ec28052e
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-04-16 21:45:59 +0530

    [LeetCode] Refactoring

--- Leetcode/Solution_915_PartitionArrayIntoDisjointSet.java
+++ Leetcode/Solution_915_PartitionArrayIntoDisjointSet.java
@@ -2,9 +2,7 @@ package Leetcode;
 
 class Solution_915_PartitionArrayIntoDisjointSet {
   public int partitionDisjoint(int[] A) {
-    int globalMax = A[0];
-    int leftMax = A[0];
-    int boundary = 0;
+    int globalMax = A[0], leftMax = A[0], boundary = 0;
 
     for (int i = 0; i < A.length; i++) {
       globalMax = A[i] > globalMax ? A[i] : globalMax;
