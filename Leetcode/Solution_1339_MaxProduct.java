b9326678ce2c812ff7944862f0b7d6a0fb781325
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-04-22 13:42:35 +0530

    Refactor

--- Leetcode/Solution_1339_MaxProduct.java
+++ Leetcode/Solution_1339_MaxProduct.java
@@ -1,14 +1,17 @@
 class Solution {
-    long res = 0, total = 0, sub;
-    public int maxProduct(TreeNode root) {
-        total = s(root); s(root);
-        return (int)(res % (int)(1e9 + 7));
-    }
+  long res = 0, total = 0, sub;
 
-    private long s(TreeNode root) {
-        if (root == null) return 0;
-        sub = root.val + s(root.left) + s(root.right);
-        res = Math.max(res, sub * (total - sub));
-        return sub;
-    }
+  public int maxProduct(TreeNode root) {
+    total = s(root);
+    s(root);
+    return (int) (res % (int) (1e9 + 7));
+  }
+
+  private long s(TreeNode root) {
+    if (root == null)
+      return 0;
+    sub = root.val + s(root.left) + s(root.right);
+    res = Math.max(res, sub * (total - sub));
+    return sub;
+  }
 }
