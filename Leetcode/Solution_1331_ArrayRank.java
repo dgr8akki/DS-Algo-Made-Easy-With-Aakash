5e4d5fa58d1b87073d43acf9a62e84d9a5e8ad05
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-04-24 13:43:30 +0530

    Refactor

--- Leetcode/Solution_1331_ArrayRank.java
+++ Leetcode/Solution_1331_ArrayRank.java
@@ -1,20 +1,21 @@
 class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-        if (arr==null || arr.length==0) return arr;
-        int[] ans = new int[arr.length];
-        Map<Integer,Integer> map = new HashMap<>();
-        int[] temp = arr.clone();
-        Arrays.sort(temp);
-        int j = 1;
-        for (int i=0;i<temp.length;i++){
-            if (!map.containsKey(temp[i])) {
-                map.put(temp[i], j++);
-            }
-        }
-        if (map.isEmpty()) return null;
-        for (int i=0;i<arr.length;i++){
-            ans[i] = map.get(arr[i]);
-        }
-        return ans;
-    }
+  public int[] arrayRankTransform(int[] arr) {
+    if (arr == null || arr.length == 0)
+      return arr;
+    int[] ans = new int[arr.length];
+    Map<Integer, Integer> map = new HashMap<>();
+    int[] temp = arr.clone();
+    Arrays.sort(temp);
+    int j = 1;
+    for (int i = 0; i < temp.length; i++)
+      if (!map.containsKey(temp[i]))
+        map.put(temp[i], j++);
+
+    if (map.isEmpty())
+      return null;
+    for (int i = 0; i < arr.length; i++)
+      ans[i] = map.get(arr[i]);
+
+    return ans;
+  }
 }
