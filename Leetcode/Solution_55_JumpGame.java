2a56d497c98dfefbcaf3957eefc32bcbb3376fc1
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-04-25 21:27:22 +0530

    [LeetCode][#55] Jump Game Memoized

--- Leetcode/Solution_55_JumpGame.java
+++ Leetcode/Solution_55_JumpGame.java
@@ -11,4 +11,20 @@ class Solution_55_JumpGame {
     }
     return max >= nums.length - 1;
   }
+
+  public boolean canJump(int[] nums) {
+    return canJump(nums, 0, new int[nums.length + 1]) == 1 ? true : false;
+  }
+
+  private int canJump(int[] nums, int current, int[] dp) {
+    if (dp[current] != 0)
+      return dp[current];
+    if (current == nums.length - 1)
+      return dp[current] = 1;
+
+    for (int i = 1; i <= nums[current]; i++)
+      if (canJump(nums, current + i, dp) == 1)
+        return dp[current] = 1;
+    return dp[current] = -1;
+  }
 }
