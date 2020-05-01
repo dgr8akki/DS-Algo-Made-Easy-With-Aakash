79ab62c081c210e5409151c08e94eaa882c40f06
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-05-01 21:10:28 +0530

    [LeetCode][MayChallenge] First Bad Version

--- Leetcode/Solution_278_FirstBadVersion.java
+++ Leetcode/Solution_278_FirstBadVersion.java
@@ -2,14 +2,31 @@ package Leetcode;
 
 public class Solution_278_FirstBadVersion {
   public int firstBadVersion(int n) {
-    int start = 1, end = n;
-    while (start < end) {
-      int mid = start + (end - start) / 2;
-      if (!isBadVersion(mid))
+    int start = 1, end = n, mid = -1;
+    Map<Integer, Boolean> map = new HashMap<>();
+
+    while (start <= end) {
+      mid = start + (end - start) / 2;
+      boolean isCurrentBad = isCurrentActuallyBad(mid, map);
+      if (isCurrentBad) {
+        if (mid > 1 && !isCurrentActuallyBad(mid - 1, map))
+          return mid;
+        if (mid == 1)
+          return mid;
+        end = mid - 1;
+      } else {
         start = mid + 1;
-      else
-        end = mid;
+      }
     }
-    return start;
+    return -1;
+  }
+
+  private boolean isCurrentActuallyBad(int mid, Map<Integer, Boolean> map) {
+    if (map.containsKey(mid)) {
+      return map.get(mid);
+    }
+    boolean current = isBadVersion(mid);
+    map.put(mid, current);
+    return current;
   }
 }
