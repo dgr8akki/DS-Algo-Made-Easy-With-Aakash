9131ad9061b6f124201fa51f80ea836af22dfd46
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-05-16 00:26:20 +0530

    [TRIJ] Refactor

--- TRIJ/ch3/Permutation.java
+++ TRIJ/ch3/Permutation.java
@@ -2,9 +2,10 @@ package com.trijs.ch3;
 
 public class Permutation {
   long permute(int n, int r) {
-//    if(n < r) return 0;
-    if(n == r) return 1;
-    if(r == 0) return 1;
+    if (n == r)
+      return 1;
+    if (r == 0)
+      return 1;
     return n * permute(n - 1, r - 1);
   }
 
