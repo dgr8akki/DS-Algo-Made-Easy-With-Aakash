68c67500e3c4205bcb24df3c8e17d5bc59ab4119
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-05-18 00:26:42 +0530

    [TRIJ] Refactor

--- TRIJ/ch3/Solution.java
+++ TRIJ/ch3/Solution.java
@@ -2,7 +2,8 @@ package com.trijs.ch3;
 
 public class Solution {
   int solve(int n, int a, int b) {
-    if(n == 0) return a;
+    if (n == 0)
+      return a;
     return solve(n - 1, b, a + b);
   }
 
