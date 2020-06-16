fa3ca407b509d0741674f73a566273b1f183dd6f
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-06-16 11:27:07 +0530

    Fix Indentation

---
+++ Algorithms/DynamicProgramming/RemoveConsecutive.java
@@ -0,0 +1,11 @@
+public class RemoveConsecutive {
+
+  private static int solve(int[] arr, int n, int index) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 3, 4, 5, 6, 7, 5 };
+    System.out.println(solve(arr, arr.length, 0));
+  }
+}
