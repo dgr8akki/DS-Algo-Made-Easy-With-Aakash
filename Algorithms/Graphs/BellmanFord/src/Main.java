110f1c721f9b9c773ce7fb92b846485c671d2c70
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-06-19 11:27:47 +0530

    Fix Indentation

--- Algorithms/Graphs/BellmanFord/src/Main.java
+++ Algorithms/Graphs/BellmanFord/src/Main.java
@@ -15,18 +15,18 @@ public class Main {
     vertexList.add(new Vertex(5));
 
     List<Edge> edgeList = new ArrayList<>();
-    edgeList.add(new Edge(vertexList.get(0),vertexList.get(1),5));
-    edgeList.add(new Edge(vertexList.get(0),vertexList.get(3),-4));
-    edgeList.add(new Edge(vertexList.get(0),vertexList.get(2),8));
-    edgeList.add(new Edge(vertexList.get(1),vertexList.get(0),-2));
-    edgeList.add(new Edge(vertexList.get(2),vertexList.get(1),-3));
-    edgeList.add(new Edge(vertexList.get(2),vertexList.get(3),9));
-    edgeList.add(new Edge(vertexList.get(3),vertexList.get(1),7));
-    edgeList.add(new Edge(vertexList.get(4),vertexList.get(0),6));
-    edgeList.add(new Edge(vertexList.get(4),vertexList.get(2),7));
+    edgeList.add(new Edge(vertexList.get(0), vertexList.get(1), 5));
+    edgeList.add(new Edge(vertexList.get(0), vertexList.get(3), -4));
+    edgeList.add(new Edge(vertexList.get(0), vertexList.get(2), 8));
+    edgeList.add(new Edge(vertexList.get(1), vertexList.get(0), -2));
+    edgeList.add(new Edge(vertexList.get(2), vertexList.get(1), -3));
+    edgeList.add(new Edge(vertexList.get(2), vertexList.get(3), 9));
+    edgeList.add(new Edge(vertexList.get(3), vertexList.get(1), 7));
+    edgeList.add(new Edge(vertexList.get(4), vertexList.get(0), 6));
+    edgeList.add(new Edge(vertexList.get(4), vertexList.get(2), 7));
 
-    BellmanFord bellmannFordAlgorithm = new BellmanFord(vertexList,edgeList);
-    bellmannFordAlgorithm.bellmanFord(vertexList.get(4),vertexList.get(3));
+    BellmanFord bellmannFordAlgorithm = new BellmanFord(vertexList, edgeList);
+    bellmannFordAlgorithm.bellmanFord(vertexList.get(4), vertexList.get(3));
 
   }
 }
