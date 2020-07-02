736503e78421db9cb63293cf5068a94b3dff2f45
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-07-02 18:11:22 +0530

    Fix Indentation

--- Algorithms/Graphs/KosarajuSCC/src/Main.java
+++ Algorithms/Graphs/KosarajuSCC/src/Main.java
@@ -17,7 +17,6 @@ public class Main {
     vertexList.add(new Vertex(6, "g"));
     vertexList.add(new Vertex(7, "h"));
 
-
     List<Edge> edgeList = new ArrayList<Edge>();
 
     edgeList.add(new Edge(1, vertexList.get(0), vertexList.get(1)));
@@ -42,22 +41,20 @@ public class Main {
     edgeList.add(new Edge(1, vertexList.get(7), vertexList.get(3)));
     edgeList.add(new Edge(1, vertexList.get(7), vertexList.get(6)));
 
-
-
     Graph graph = new Graph(edgeList, vertexList);
 
-//		for(Vertex v : graph.getTransposeGraph().getVertexList()){
-//			for(Vertex u : v.getAdjaenciesList()){
-//				System.out.println(v+"->"+u);
-//			}
-//		}
+    // for(Vertex v : graph.getTransposeGraph().getVertexList()){
+    // for(Vertex u : v.getAdjaenciesList()){
+    // System.out.println(v+"->"+u);
+    // }
+    // }
 
     Kosaraju kosarajuAlgorithm = new Kosaraju(graph);
 
     System.out.println(kosarajuAlgorithm.getCount());
 
-    for(Vertex vertex : vertexList){
-      System.out.println(vertex+" - "+vertex.getComponentId());
+    for (Vertex vertex : vertexList) {
+      System.out.println(vertex + " - " + vertex.getComponentId());
     }
 
   }
