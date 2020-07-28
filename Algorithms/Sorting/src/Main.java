9e89f82c12273d580284dff158790121442da344
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-07-28 20:14:00 +0530

    Fixing indentation

--- Algorithms/Sorting/src/Main.java
+++ Algorithms/Sorting/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    int[] arr = {10,11,-1,2,5,45};
+    int[] arr = { 10, 11, -1, 2, 5, 45 };
     new BogoSort(Arrays.copyOf(arr, arr.length)).sort();
     new BubbleSort(Arrays.copyOf(arr, arr.length)).sort();
     new SelectionSort(Arrays.copyOf(arr, arr.length)).sort();
