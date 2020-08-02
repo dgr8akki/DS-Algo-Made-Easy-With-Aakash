8d34a1e18989a4cc952fb0ee15e4bc86a1b8dc91
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-02 20:16:14 +0530

    Fixing indentation

--- Algorithms/HashTable/LinearProbingHashTable/src/Main.java
+++ Algorithms/HashTable/LinearProbingHashTable/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
   public static void main(String[] args) {
     LinearProbingHashTable hashTable = new LinearProbingHashTable();
-    hashTable.put(13,12);
-    hashTable.put(23,22);
-    hashTable.put(3,24);
-    hashTable.put(43,25);
+    hashTable.put(13, 12);
+    hashTable.put(23, 22);
+    hashTable.put(3, 24);
+    hashTable.put(43, 25);
     System.out.println(hashTable.get(3));
     System.out.println(hashTable.get(13));
   }
