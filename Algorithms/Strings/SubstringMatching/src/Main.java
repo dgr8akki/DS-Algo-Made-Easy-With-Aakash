52848186519600205b0a442e2ebbe975b373e518
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-04 20:16:42 +0530

    Fixing indentation

--- Algorithms/Strings/SubstringMatching/src/Main.java
+++ Algorithms/Strings/SubstringMatching/src/Main.java
@@ -3,7 +3,7 @@ package Algorithms.Strings.SubstringMatching.src;
 public class Main {
   public static void main(String[] args) {
     String actual = "The Quick Brown Fox Jumps over the little lazy dog.";
-    String pattern= "over";
+    String pattern = "over";
     BruteForce bruteForce = new BruteForce();
     System.out.println(bruteForce.contains(actual, pattern));
   }
