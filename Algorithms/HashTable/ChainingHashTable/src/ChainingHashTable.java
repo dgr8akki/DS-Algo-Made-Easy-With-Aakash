af7b9387abf2fb3468c39d8394abf2f7fb5d0c8f
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-09 20:17:21 +0530

    Fixing indentation

--- Algorithms/HashTable/ChainingHashTable/src/ChainingHashTable.java
+++ Algorithms/HashTable/ChainingHashTable/src/ChainingHashTable.java
@@ -8,14 +8,14 @@ class ChainingHashTable {
   }
 
   private int hash(int key) {
-    if(key > -1)
+    if (key > -1)
       return key % Constants.CAPACITY;
     return 0;
   }
 
   void put(int key, int val) {
     int hashedKeyIndex = hash(key);
-    if(map[hashedKeyIndex] == null) {
+    if (map[hashedKeyIndex] == null) {
       map[hashedKeyIndex] = new HashItem(key, val);
     } else {
       HashItem temp = map[hashedKeyIndex];
@@ -28,11 +28,11 @@ class ChainingHashTable {
 
   int get(int key) {
     int hashedKeyIndex = hash(key);
-    if(map[hashedKeyIndex] == null)
+    if (map[hashedKeyIndex] == null)
       return -1;
     HashItem temp = map[hashedKeyIndex];
     while (temp != null) {
-      if(temp.getKey() == key)
+      if (temp.getKey() == key)
         return temp.getValue();
       temp = temp.getNext();
     }
