9efa4247c786ed7bcef6ca8101c35891ca08425f
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-13 20:09:57 +0530

    Code Revamp

--- DataStructures/Intro/BinarySearch.java
+++ DataStructures/Intro/BinarySearch.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
  */
 public class BinarySearch {
 
-    public static int search(int[] a, int target) {
-        int start = 0, n = a.length, end = n - 1;
+  public static int search(int[] a, int target) {
+    int start = 0, n = a.length, end = n - 1;
 
-        while (start <= end) {
-            int mid = start + ((end - start) / 2);
+    while (start <= end) {
+      int mid = start + ((end - start) / 2);
 
-            if (a[mid] == target)
-                return mid;
+      if (a[mid] == target)
+        return mid;
 
-            if (a[mid] < target)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
-        return -1;
+      if (a[mid] < target)
+        start = mid + 1;
+      else
+        end = mid - 1;
     }
+    return -1;
+  }
 
-    public static void main(String[] args) {
-        int[] ar = { 1, 3, 4, 5, 12, 20 };
-        System.out.println(Arrays.toString(ar));
-        System.out.println(search(ar, 1));
-    }
+  public static void main(String[] args) {
+    int[] ar = { 1, 3, 4, 5, 12, 20 };
+    System.out.println(Arrays.toString(ar));
+    System.out.println(search(ar, 1));
+  }
 }
