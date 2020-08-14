44ca81c80e9d9b31daf8228efdcae66a456e5a6c
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-14 20:10:06 +0530

    Code Revamp

--- DataStructures/Intro/Euclid.java
+++ DataStructures/Intro/Euclid.java
@@ -3,15 +3,15 @@
  */
 public class Euclid {
 
-    public static int gcd(int a, int b) {
-        if (b > a)
-            return gcd(b, a);
-        if (b == 0)
-            return a;
-        return gcd(b, a % b);
-    }
+  public static int gcd(int a, int b) {
+    if (b > a)
+      return gcd(b, a);
+    if (b == 0)
+      return a;
+    return gcd(b, a % b);
+  }
 
-    public static void main(String[] args) {
-        System.out.println(gcd(11, 2));
-    }
+  public static void main(String[] args) {
+    System.out.println(gcd(11, 2));
+  }
 }
