2939f2b2d4f3b8df7e47c70f2fdb4583c996adca
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-17 20:10:38 +0530

    Code Revamp

--- DataStructures/Intro/SelectionSort.java
+++ DataStructures/Intro/SelectionSort.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
  */
 public class SelectionSort {
 
-    public static void sorter(int[] a) {
-        int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (a[j] < a[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            swap(minIndex, i, a);
+  public static void sorter(int[] a) {
+    int n = a.length;
+    for (int i = 0; i < n - 1; i++) {
+      int minIndex = i;
+      for (int j = i + 1; j < n; j++) {
+        if (a[j] < a[minIndex]) {
+          minIndex = j;
         }
-    }
+      }
 
-    public static void swap(int i, int j, int[] ar) {
-        int temp = ar[i];
-        ar[i] = ar[j];
-        ar[j] = temp;
+      swap(minIndex, i, a);
     }
+  }
 
-    public static void main(String[] args) {
-        int[] ar = { 1, 3, 4, 5, 2, 0 };
-        System.out.println(Arrays.toString(ar));
-        sorter(ar);
-        System.out.println(Arrays.toString(ar));
-    }
+  public static void swap(int i, int j, int[] ar) {
+    int temp = ar[i];
+    ar[i] = ar[j];
+    ar[j] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[] ar = { 1, 3, 4, 5, 2, 0 };
+    System.out.println(Arrays.toString(ar));
+    sorter(ar);
+    System.out.println(Arrays.toString(ar));
+  }
 }
