2c1618e60d800cd8be8ef1a184b0eb353facb75c
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-08-18 20:10:48 +0530

    Code Revamp

--- DataStructures/Intro/LinearSearch.java
+++ DataStructures/Intro/LinearSearch.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
  */
 public class LinearSearch {
 
-    public static int search(int[] a, int target) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == target)
-                return i;
-        }
-        return -1;
+  public static int search(int[] a, int target) {
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] == target)
+        return i;
     }
+    return -1;
+  }
 
-    public static void main(String[] args) {
-        int[] ar = { 1, 3, 4, 5, 2, 0 };
-        System.out.println(Arrays.toString(ar));
-        System.out.println(search(ar, 0));
-    }
+  public static void main(String[] args) {
+    int[] ar = { 1, 3, 4, 5, 2, 0 };
+    System.out.println(Arrays.toString(ar));
+    System.out.println(search(ar, 0));
+  }
 }
