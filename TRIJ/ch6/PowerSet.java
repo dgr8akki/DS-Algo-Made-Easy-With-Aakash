752a3da5937de370d4a36fd59d50cbe2457e0017
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-09-02 22:48:47 +0530

    Code Refactoring

--- TRIJ/ch6/PowerSet.java
+++ TRIJ/ch6/PowerSet.java
@@ -4,14 +4,16 @@ public class PowerSet {
   void generate(String s) {
     powerSet(s, 0, "");
   }
+
   private void powerSet(String givenString, int index, String currentString) {
-    if(index == givenString.length()) {
-      System.out.println("{" +currentString+"}");
+    if (index == givenString.length()) {
+      System.out.println("{" + currentString + "}");
       return;
     }
     powerSet(givenString, index + 1, currentString + givenString.charAt(index));
     powerSet(givenString, index + 1, currentString);
   }
+
   public static void main(String[] args) {
     PowerSet powerSet = new PowerSet();
     powerSet.generate("ABV");
