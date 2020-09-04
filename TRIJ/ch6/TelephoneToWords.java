295ec35d49ef95ecf515149fc2e893e881339f18
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-09-04 22:49:12 +0530

    Code RefactDoring

--- TRIJ/ch6/TelephoneToWords.java
+++ TRIJ/ch6/TelephoneToWords.java
@@ -5,18 +5,18 @@ import java.util.Map;
 
 public class TelephoneToWords {
   void generate(String number) {
-    if(number.length() == 0)
+    if (number.length() == 0)
       return;
 
     Map<Character, char[]> map = new HashMap<>();
-    map.put('2', new char[]{'a','b','c'});
-    map.put('3', new char[]{'d','e','f'});
-    map.put('4', new char[]{'g','h','i'});
-    map.put('5', new char[]{'j','k','l'});
-    map.put('6', new char[]{'m','n','o'});
-    map.put('7', new char[]{'p','s','r'});
-    map.put('8', new char[]{'v','t','u'});
-    map.put('9', new char[]{'y','w','x'});
+    map.put('2', new char[] { 'a', 'b', 'c' });
+    map.put('3', new char[] { 'd', 'e', 'f' });
+    map.put('4', new char[] { 'g', 'h', 'i' });
+    map.put('5', new char[] { 'j', 'k', 'l' });
+    map.put('6', new char[] { 'm', 'n', 'o' });
+    map.put('7', new char[] { 'p', 's', 'r' });
+    map.put('8', new char[] { 'v', 't', 'u' });
+    map.put('9', new char[] { 'y', 'w', 'x' });
     generate(number, 0, "", map);
   }
 
@@ -26,7 +26,7 @@ public class TelephoneToWords {
       return;
     }
 
-    if(number.charAt(index) == '1' || number.charAt(index) == '0')
+    if (number.charAt(index) == '1' || number.charAt(index) == '0')
       return;
 
     for (char currentChar : map.get(number.charAt(index)))
