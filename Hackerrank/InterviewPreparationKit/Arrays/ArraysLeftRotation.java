911db11cbfbcf74b3a409b8c0f27e4216464c363
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-09-07 19:03:41 +0530

    Fix code indentations

--- Hackerrank/InterviewPreparationKit/Arrays/ArraysLeftRotation.java
+++ Hackerrank/InterviewPreparationKit/Arrays/ArraysLeftRotation.java
@@ -25,8 +25,7 @@ public class ArraysLeftRotation {
   }
 
   public static void main(String[] args) throws IOException {
-    BufferedWriter bufferedWriter =
-        new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
     String[] nd = scanner.nextLine().split(" ");
 
