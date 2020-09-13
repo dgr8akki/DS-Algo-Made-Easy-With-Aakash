fe6dbe7920bc81c1962686ce14c3a2a2cd911bb3
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-09-13 19:04:53 +0530

    Fix code indentations

--- Practise/QuickSort.java
+++ Practise/QuickSort.java
@@ -9,7 +9,8 @@ public class QuickSort {
   }
 
   private static void quickSortUtil(int[] arr, int start, int end) {
-    if(start >= end) return;
+    if (start >= end)
+      return;
     int pivot = partition(arr, start, end);
     quickSortUtil(arr, 0, pivot - 1);
     quickSortUtil(arr, pivot + 1, end);
@@ -18,8 +19,8 @@ public class QuickSort {
   private static int partition(int[] arr, int start, int end) {
     swap(arr, (start + end) / 2, end);
     int leftRunner = start;
-    for(int rightRunner = start; rightRunner < end; rightRunner++) {
-      if(arr[rightRunner] < arr[end]) {
+    for (int rightRunner = start; rightRunner < end; rightRunner++) {
+      if (arr[rightRunner] < arr[end]) {
         swap(arr, leftRunner, rightRunner);
         leftRunner++;
       }
@@ -34,8 +35,9 @@ public class QuickSort {
     arr[i] = arr[j];
     arr[j] = temp;
   }
+
   public static void main(String[] args) {
-    int[] a = {1,2,9,8,7,5,3};
+    int[] a = { 1, 2, 9, 8, 7, 5, 3 };
     System.out.println("Array before sorted: " + Arrays.toString(a));
     System.out.println("Array After sorted: " + Arrays.toString(sort(a)));
   }
