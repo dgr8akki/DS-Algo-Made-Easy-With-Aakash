c94a1562a9d6618dcf8497d2646ee61ab4023f28
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-10-09 19:43:13 +0530

    [CR] Refactoring code

--- GFG/DynamicProgramming/BinomialCoefficient.java
+++ GFG/DynamicProgramming/BinomialCoefficient.java
@@ -1,26 +1,25 @@
-class BinomialCoefficient { 
-  static long binomialCoeff(int n, int k) { 
-    long res = 1; 
-    
-    if (k > n - k) 
-      k = n - k; 
-    
-    for (int i = 0; i < k; ++i) { 
-      res *= (n - i); 
-      res /= (i + 1); 
-    } 
-    
-    return res; 
+class BinomialCoefficient {
+  static long binomialCoeff(int n, int k) {
+    long res = 1;
+
+    if (k > n - k)
+      k = n - k;
+
+    for (int i = 0; i < k; ++i) {
+      res *= (n - i);
+      res /= (i + 1);
+    }
+
+    return res;
   }
-    
-  static long catalan(int n) { 
-    long c = binomialCoeff(2 * n, n); 
-    return c / (n + 1); 
+
+  static long catalan(int n) {
+    long c = binomialCoeff(2 * n, n);
+    return c / (n + 1);
   }
-    
-  public static void main(String[] args) { 
+
+  public static void main(String[] args) {
     for (int i = 0; i < 10; i++)
       System.out.print(catalan(i) + " ");
-  } 
-} 
-    
+  }
+}
