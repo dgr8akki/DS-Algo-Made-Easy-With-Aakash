b99054c64519ea964697df192d60597a3bb3a5ff
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-10-14 19:43:59 +0530

    [CR] Refactoring code

--- Leetcode/Solution_9_PalindromeNumber.java
+++ Leetcode/Solution_9_PalindromeNumber.java
@@ -3,10 +3,10 @@ package Leetcode;
 class Solution_9_PalindromeNumber {
   public boolean isPalindrome(int x) {
     String n = Integer.toString(x);
-    for (int i = 0; i < n.length() / 2; i++) {
+    for (int i = 0; i < n.length() / 2; i++)
       if (n.charAt(i) != n.charAt(n.length() - 1 - i))
         return false;
-    }
+
     return true;
   }
 }
