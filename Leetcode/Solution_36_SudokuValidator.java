d1788e94f00ada2c00baccdf5dd6f829c26828ca
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-10-19 19:51:10 +0530

    [CR] Refactoring code

--- Leetcode/Solution_36_SudokuValidator.java
+++ Leetcode/Solution_36_SudokuValidator.java
@@ -3,7 +3,7 @@ package Leetcode;
 class Solution_36_SudokuValidator {
   public boolean isValidSudoku(char[][] board) {
     Set seen = new HashSet();
-    for (int i = 0; i < 9; ++i) {
+    for (int i = 0; i < 9; ++i)
       for (int j = 0; j < 9; ++j) {
         char number = board[i][j];
         if (number != '.')
@@ -11,7 +11,6 @@ class Solution_36_SudokuValidator {
               || !seen.add(number + " in block " + i / 3 + "-" + j / 3))
             return false;
       }
-    }
     return true;
   }
 }
